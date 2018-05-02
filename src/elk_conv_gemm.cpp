3e024b8c97b4560764fb10c47aab8e06c08dca40
Yong Wu <yong.wu@intel.com>
2018-05-02 10:01:52 +0800

    conv:wino: gemm kernel with T=1...28
    Signed-off-by: Yong Wu <yong.wu@intel.com>

--- src/elk_conv_gemm.cpp
+++ src/elk_conv_gemm.cpp
@@ -59,29 +59,29 @@ BOOST_PP_REPEAT_FROM_TO(1, 29, DEF_C_elk_gemm, xxx);
 #define AVX512_DEF(z, n, xxx) __m512 t##n;
 #define AVX512_ZERO(z, n, xxx) t##n = _mm512_setzero_ps();
 #define AVX512_LOAD(z, n, xxx) t##n = _mm512_load_ps(&atoutput(_O2, n, 0));
+#define AVX512_STORE(z, n, xxx) _mm512_store_ps(&atoutput(_O2, n, 0), t##n);
 #define AVX512_FMA(z, n, xxx)            \
   x = _mm512_set1_ps(*(x_ptr + n * 16)); \
   t##n = _mm512_fmadd_ps(w, x, t##n);
-#define STORE(z, n, xxx) _mm512_store_ps(&atoutput(_O2, n, 0), t##n);
 
-#define DEF_V_elk_gemm(z, n, xxx)                                             \
+#define DEF_V_elk_gemm(z, T, xxx)                                             \
   template <>                                                                 \
-  void elk_gemm<float, n, 16, ISA_SKX_AVX512>(                                \
+  void elk_gemm<float, T, 16, ISA_SKX_AVX512>(                                \
       elx_conv_t<float> & xc, float *toutput, float *tinput, float *tweights, \
       bool zero_out) {                                                        \
     ENABLE_AVX512F();                                                         \
                                                                               \
     mdarray<float, 4> atweights(tweights, xc.O2, xc.I2, 16, 16);              \
-    mdarray<float, 3> atinput(tinput, xc.I2, n, 16);                          \
-    mdarray<float, 3> atoutput(toutput, xc.O2, n, 16);                        \
+    mdarray<float, 3> atinput(tinput, xc.I2, T, 16);                          \
+    mdarray<float, 3> atoutput(toutput, xc.O2, T, 16);                        \
                                                                               \
     for (int _O2 = 0; _O2 < xc.O2; ++_O2) {                                   \
-      BOOST_PP_REPEAT(n, AVX512_DEF, xxx)                                     \
+      BOOST_PP_REPEAT(T, AVX512_DEF, xxx)                                     \
                                                                               \
       if (zero_out) {                                                         \
-        BOOST_PP_REPEAT(n, AVX512_ZERO, xxx)                                  \
+        BOOST_PP_REPEAT(T, AVX512_ZERO, xxx)                                  \
       } else {                                                                \
-        BOOST_PP_REPEAT(n, AVX512_LOAD, xxx)                                  \
+        BOOST_PP_REPEAT(T, AVX512_LOAD, xxx)                                  \
       }                                                                       \
                                                                               \
       for (int _I2 = 0; _I2 < xc.I2; ++_I2) {                                 \
@@ -89,10 +89,10 @@ BOOST_PP_REPEAT_FROM_TO(1, 29, DEF_C_elk_gemm, xxx);
           __m512 x;                                                           \
           __m512 w = _mm512_load_ps(&atweights(_O2, _I2, _V, 0));             \
           float *x_ptr = &atinput(_I2, 0, _V);                                \
-          BOOST_PP_REPEAT(n, AVX512_FMA, xxx)                                 \
+          BOOST_PP_REPEAT(T, AVX512_FMA, xxx)                                 \
         }                                                                     \
       }                                                                       \
-      BOOST_PP_REPEAT(n, STORE, xxx)                                          \
+      BOOST_PP_REPEAT(T, AVX512_STORE, xxx)                                   \
     }                                                                         \
   }
 
