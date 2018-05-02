08c22e18777a23fc395f7e1e830b04a47a4ac2f4
Yong Wu <yong.wu@intel.com>
2018-05-02 09:56:15 +0800

    conv:wino:gemm kernel with T=1...28
    Signed-off-by: Yong Wu <yong.wu@intel.com>

--- src/elk_conv_gemm.cpp
+++ src/elk_conv_gemm.cpp
@@ -1,9 +1,16 @@
+#include <boost/preprocessor/repetition/repeat.hpp>
+#include <boost/preprocessor/repetition/repeat_from_to.hpp>
 #include <assert.h>
 #include <x86intrin.h>
 #include "el_def.hpp"
 #include "el_utils.hpp"
 #include "elx_conv.hpp"
 #include "elk_conv.hpp"
+
+// blocking -
+// oc3, ic3, A * A, O2, I2, V, V
+// t2, A*A, ic3, I2, T, V
+// t2, A*A, oc3, O2, T, V
 
 namespace euler {
 
@@ -39,58 +46,56 @@ void elk_gemm(elx_conv_t<Type> &xc, Type *toutput, Type *tinput, Type *tweights,
   }
 }
 
-template <>
-void elk_gemm<float, 25, 16, ISA_GENERIC>(elx_conv_t<float> &xc, float *toutput,
-                                          float *tinput, float *tweights,
-                                          bool zero_out) {
-  elk_gemm<float, 25, 16>(xc, toutput, tinput, tweights, zero_out);
-}
+#define DEF_C_elk_gemm(z, n, data)                                           \
+  template <>                                                                \
+  void elk_gemm<float, n, 16, ISA_GENERIC>(elx_conv_t<float> & xc,           \
+                                           float *toutput, float *tinput,    \
+                                           float *tweights, bool zero_out) { \
+    elk_gemm<float, n, 16>(xc, toutput, tinput, tweights, zero_out);         \
+  }
 
-template <>
-void elk_gemm<float, 25, 16, ISA_SKX_AVX512>(elx_conv_t<float> &xc,
-                                             float *toutput, float *tinput,
-                                             float *tweights, bool zero_out) {
-  ENABLE_AVX512F();
+BOOST_PP_REPEAT_FROM_TO(1, 29, DEF_C_elk_gemm, xxx);
 
-  mdarray<float, 4> atweights(tweights, xc.O2, xc.I2, 16, 16);
-  mdarray<float, 3> atinput(tinput, xc.I2, 25, 16);
-  mdarray<float, 3> atoutput(toutput, xc.O2, 25, 16);
-
-  for (int _O2 = 0; _O2 < xc.O2; ++_O2) {
-#undef OP
-#define OP(x) __m512 t##x
-    OP_0_to_24();
-
-    if (zero_out) {
-#undef OP
-#define OP(x) t##x = _mm512_setzero_ps();
-      OP_0_to_24();
-    } else {
-#undef OP
-#define OP(x) t##x = _mm512_load_ps(&atoutput(_O2, x, 0));
-      OP_0_to_24();
-    }
-
-    for (int _I2 = 0; _I2 < xc.I2; ++_I2) {
-      for (int _V = 0; _V < 16; ++_V) {
-        __m512 x;
-        __m512 w = _mm512_load_ps(&atweights(_O2, _I2, _V, 0));
-        float *x_ptr = &atinput(_I2, 0, _V);
-#undef OP
-#define OP(n)                            \
+#define AVX512_DEF(z, n, xxx) __m512 t##n;
+#define AVX512_ZERO(z, n, xxx) t##n = _mm512_setzero_ps();
+#define AVX512_LOAD(z, n, xxx) t##n = _mm512_load_ps(&atoutput(_O2, n, 0));
+#define AVX512_FMA(z, n, xxx)            \
   x = _mm512_set1_ps(*(x_ptr + n * 16)); \
   t##n = _mm512_fmadd_ps(w, x, t##n);
-        OP_0_to_24();
-      }
-    }
-#undef OP
-#define OP(n) _mm512_store_ps(&atoutput(_O2, n, 0), t##n);
-    OP_0_to_24();
-  }
-}
+#define STORE(z, n, xxx) _mm512_store_ps(&atoutput(_O2, n, 0), t##n);
 
-// elk_trans_weights
-// oc3, ic3, A * A, O2, I2, V, V
-// t2, A*A, ic3, I2, T, V
-// t2, A*A, oc3, O2, T, V
+#define DEF_V_elk_gemm(z, n, xxx)                                             \
+  template <>                                                                 \
+  void elk_gemm<float, n, 16, ISA_SKX_AVX512>(                                \
+      elx_conv_t<float> & xc, float *toutput, float *tinput, float *tweights, \
+      bool zero_out) {                                                        \
+    ENABLE_AVX512F();                                                         \
+                                                                              \
+    mdarray<float, 4> atweights(tweights, xc.O2, xc.I2, 16, 16);              \
+    mdarray<float, 3> atinput(tinput, xc.I2, n, 16);                          \
+    mdarray<float, 3> atoutput(toutput, xc.O2, n, 16);                        \
+                                                                              \
+    for (int _O2 = 0; _O2 < xc.O2; ++_O2) {                                   \
+      BOOST_PP_REPEAT(n, AVX512_DEF, xxx)                                     \
+                                                                              \
+      if (zero_out) {                                                         \
+        BOOST_PP_REPEAT(n, AVX512_ZERO, xxx)                                  \
+      } else {                                                                \
+        BOOST_PP_REPEAT(n, AVX512_LOAD, xxx)                                  \
+      }                                                                       \
+                                                                              \
+      for (int _I2 = 0; _I2 < xc.I2; ++_I2) {                                 \
+        for (int _V = 0; _V < 16; ++_V) {                                     \
+          __m512 x;                                                           \
+          __m512 w = _mm512_load_ps(&atweights(_O2, _I2, _V, 0));             \
+          float *x_ptr = &atinput(_I2, 0, _V);                                \
+          BOOST_PP_REPEAT(n, AVX512_FMA, xxx)                                 \
+        }                                                                     \
+      }                                                                       \
+      BOOST_PP_REPEAT(n, STORE, xxx)                                          \
+    }                                                                         \
+  }
+
+BOOST_PP_REPEAT_FROM_TO(1, 29, DEF_V_elk_gemm, xxx)
+
 }  // namespace euler
