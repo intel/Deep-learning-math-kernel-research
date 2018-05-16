8e5c6f0c8ffc74a3de3432fe743d6fcf3b984071
Yong Wu <yong.wu@intel.com>
2018-05-17 03:08:52 +0800

    test: enhance test script
    Signed-off-by: Yong Wu <yong.wu@intel.com>

--- script/run.sh
+++ script/run.sh
@@ -10,18 +10,15 @@ echo Release Build...
 sockets=$( lscpu | grep 'Socket(s)' | cut -d: -f2 )
 cores_per_socket=$( lscpu | grep 'Core(s) per socket' | cut -d: -f2 )
 cores=$(( sockets  * cores_per_socket ))
+OMP_ENV="OMP_NUM_THREADS=$(( cores )) \
+  KMP_HW_SUBSET=$(( sockets ))s,$(( cores_per_socket ))c,1t \
+  KMP_AFFINITY=compact,granularity=fine \
+  KMP_BLOCKTIME=infinite"
+echo OMP Environment: $OMP_ENV
 
 echo Run convolution test...
-OMP_NUM_THREADS=$(( cores ))                              \
-KMP_HW_SUBSET=$(( sockets ))s,$(( cores_per_socket ))c,1t \
-KMP_AFFINITY=compact,granularity=fine                     \
-KMP_BLOCKTIME=infinite                                    \
-$ROOT_DIR/build/release/bin/elt_conv 
+eval $OMP_ENV $ROOT_DIR/build/release/bin/elt_conv
 
 echo Run unit tests...
-OMP_NUM_THREADS=$(( cores ))                              \
-KMP_HW_SUBSET=$(( sockets ))s,$(( cores_per_socket ))c,1t \
-KMP_AFFINITY=compact,granularity=fine                     \
-KMP_BLOCKTIME=infinite                                    \
-$ROOT_DIR/build/release/bin/elt_unitests -p -t
+eval $OMP_ENV $ROOT_DIR/build/release/bin/elt_unitests -t
 
