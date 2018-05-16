a718b4b37929757204eaea4e305aac62a7550f3c
Yong Wu <yong.wu@intel.com>
2018-05-17 03:01:00 +0800

    test: enhance test script
    Signed-off-by: Yong Wu <yong.wu@intel.com>

--- script/run.sh
+++ script/run.sh
@@ -7,17 +7,21 @@ echo
 echo Release Build...
 #cd "$ROOT_DIR" && make distclean && make -j all && cd -
 
+sockets=$( lscpu | grep 'Socket(s)' | cut -d: -f2 )
+cores_per_socket=$( lscpu | grep 'Core(s) per socket' | cut -d: -f2 )
+cores=$(( sockets  * cores_per_socket ))
+
 echo Run convolution test...
-OMP_NUM_THREADS=18 \
-KMP_HW_SUBSET=1s,18c,1t \
-KMP_AFFINITY=compact,granularity=fine \
-KMP_BLOCKTIME=infinite \
+OMP_NUM_THREADS=$(( cores ))                              \
+KMP_HW_SUBSET=$(( sockets ))s,$(( cores_per_socket ))c,1t \
+KMP_AFFINITY=compact,granularity=fine                     \
+KMP_BLOCKTIME=infinite                                    \
 $ROOT_DIR/build/release/bin/elt_conv 
 
 echo Run unit tests...
-OMP_NUM_THREADS=18 \
-KMP_HW_SUBSET=1s,18c,1t \
-KMP_AFFINITY=compact,granularity=fine \
-KMP_BLOCKTIME=infinite \
+OMP_NUM_THREADS=$(( cores ))                              \
+KMP_HW_SUBSET=$(( sockets ))s,$(( cores_per_socket ))c,1t \
+KMP_AFFINITY=compact,granularity=fine                     \
+KMP_BLOCKTIME=infinite                                    \
 $ROOT_DIR/build/release/bin/elt_unitests -p -t
 
