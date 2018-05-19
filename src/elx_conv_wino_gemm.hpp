e22855228a1913b494cb9bcb5b55949390ca80d6
Yong Wu <yong.wu@intel.com>
2018-05-19 22:04:19 +0800

    conv:wino: enhance kernel function pointer
    Signed-off-by: Yong Wu <yong.wu@intel.com>

--- src/elx_conv_wino_gemm.hpp
+++ src/elx_conv_wino_gemm.hpp
@@ -17,36 +17,35 @@ class elx_conv_wino_gemm_t : public elx_conv_t<Type> {
 
   virtual void execute(Type *input, Type *weights, Type *output, Type *bias);
 
- private:
+  private:
   void trans_weights(Type *tweights, Type *weights);
   void trans_input(Type *tinput, Type *input, int _t2);
   void trans_output(Type *output, Type *toutput, Type *bias, int _t2);
   void gemm(Type *toutput, Type *tinput, Type *tweights);
 
+  decltype(
+      convolution_winograd_kernel<Type, 0, A, K, V, I, false>::gemm)
+      *ker_gemm_;
+  decltype(
+      convolution_winograd_kernel<Type, 0, A, K, V, I, false>::trans_input)
+      *ker_trans_input_;
+  decltype(
+      convolution_winograd_kernel<Type, 0, A, K, V, I, false>::trans_input0)
+      *ker_trans_input0_;
+  decltype(
+      convolution_winograd_kernel<Type, 0, A, K, V, I, false>::trans_weights)
+      *ker_trans_weights_;
+  decltype(
+      convolution_winograd_kernel<Type, 0, A, K, V, I, false>::trans_output)
+      *ker_trans_output_;
+  decltype(
+      convolution_winograd_kernel<Type, 0, A, K, V, I, false>::trans_output0)
+      *ker_trans_output0_;
+
   size_t mthr_;
   Type *tweights_;
   Type *tinput_;
   Type *toutput_;
-
-  using func_gemm_t          = decltype(
-      convolution_winograd_kernel<Type, 1, 0, 0, V, I, false>::gemm);
-  using func_trans_input_t   = decltype(
-      convolution_winograd_kernel<Type, 0, A, K, V, I, false>::trans_input);
-  using func_trans_input0_t  = decltype(
-      convolution_winograd_kernel<Type, 0, A, K, V, I, false>::trans_input0);
-  using func_trans_weights_t = decltype(
-      convolution_winograd_kernel<Type, 0, A, K, V, I, false>::trans_weights);
-  using func_trans_output_t  = decltype(
-      convolution_winograd_kernel<Type, 0, A, K, V, I, true>::trans_output);
-  using func_trans_output0_t = decltype(
-      convolution_winograd_kernel<Type, 0, A, K, V, I, true>::trans_output0);
-
-  func_gemm_t *ker_gemm_;
-  func_trans_input_t *ker_trans_input_;
-  func_trans_input0_t *ker_trans_input0_;
-  func_trans_weights_t *ker_trans_weights_;
-  func_trans_output_t *ker_trans_output_;
-  func_trans_output0_t *ker_trans_output0_;
 };
 
 template class elx_conv_wino_gemm_t<float, 5, 3, 16, ISA_GENERIC>;
