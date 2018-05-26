4bb761864621f5d0880f566cffe6d80e1db94291
Yong Wu <yong.wu@intel.com>
2018-05-27 03:56:10 +0800

    test: enhance conv test script
    Signed-off-by: Yong Wu <yong.wu@intel.com>

--- script/run.sh
+++ script/run.sh
@@ -5,11 +5,17 @@ ROOT_DIR="$(dirname $(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd))"
 echo Root dir: $ROOT_DIR
 echo
 
-sockets=$( lscpu | grep 'Socket(s)' | cut -d: -f2 )
-cores_per_socket=$( lscpu | grep 'Core(s) per socket' | cut -d: -f2 )
-cores=$(( sockets  * cores_per_socket ))
-OMP_ENV="OMP_NUM_THREADS=$(( cores )) \
-  KMP_HW_SUBSET=$(( sockets ))s,$(( cores_per_socket ))c,1t \
+num_sockets=
+cores_per_socket=
+threads_per_core=
+num_threads=
+
+${num_sockets:=$( lscpu | grep 'Socket(s)' | cut -d: -f2 )}
+${cores_per_socket:=$( lscpu | grep 'Core(s) per socket' | cut -d: -f2 )}
+${num_threads:=$(( num_sockets  * cores_per_socket ))}
+
+OMP_ENV="OMP_NUM_THREADS=$(( num_threads )) \
+  KMP_HW_SUBSET=$(( num_sockets ))s,$(( cores_per_socket ))c,${threads_per_core}t \
   KMP_AFFINITY=compact,granularity=fine \
   KMP_BLOCKTIME=infinite"
 echo OMP Environment: $OMP_ENV
@@ -19,9 +25,36 @@ function build() {
 }
 
 function conv_test() {
+  # Default
+  n=1; i=64; o=64; h=224; w=224; H=224; W=224; k=3; K=3; p=1; P=1; s=1; S=1
+  b=1; r=0; v=1; a=wino
+
+  OPTIND=1
+  while getopts ":n:i:o:h:w:H:W:k:K:p:P:s:S:b:r:v:a:" opt; do
+    case "$opt" in
+      n) n=$OPTARG ;;
+      i) i=$OPTARG ;;
+      o) o=$OPTARG ;;
+      h) h=$OPTARG ;;
+      w) w=$OPTARG ;;
+      H) H=$OPTARG ;;
+      W) W=$OPTARG ;;
+      k) k=$OPTARG ;;
+      K) K=$OPTARG ;;
+      p) p=$OPTARG ;;
+      P) P=$OPTARG ;;
+      s) s=$OPTARG ;;
+      S) S=$OPTARG ;;
+      b) b=$OPTARG ;;
+      r) r=$OPTARG ;;
+      v) v=$OPTARG ;;
+      a) a=$OPTARG ;;
+    esac
+  done
+  shift $((OPTIND-1))
   eval $OMP_ENV $ROOT_DIR/build/release/bin/elt_conv \
-    -n1 -i64 -o64 -h224 -w224 -H224 -W224 -k3 -K3 -p1 -P1 -s1 -S1 \
-    -b1 -r0 -v1 -awino
+    -n$n -i$i -o$o -h$h -w$w -H$H -W$W -k$k -K$K -p$p -P$P -s$s -S$S \
+    -b$b -r$r -v$v -a$a
 }
 
 function unit_test() {
@@ -41,7 +74,7 @@ Euler test script:
 }
 
 OPTIND=1
-while getopts "hbuc" opt; do
+while getopts ":hbuc" opt; do
   case "$opt" in
     h)
       show_help
@@ -53,14 +86,17 @@ while getopts "hbuc" opt; do
       ;;
     c)
       echo Run convolution test...
-      conv_test
+      shift
+      conv_test $@
+      exit 0
       ;;
     u)
       echo Run unit tests...
       unit_test
       ;;
-    *)
+    \?)
       show_help
+      exit 0
       ;;
   esac
 done
