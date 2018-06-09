66973a0fe25b17079d0b5a3d15ea1ac3223bb615
Yong Wu <yong.wu@intel.com>
2018-06-10 00:33:43 +0900

    script: add n64 config
    Signed-off-by: Yong Wu <yong.wu@intel.com>

--- script/best_config.sh
+++ script/best_config.sh
@@ -3,7 +3,7 @@
 m=$1
 # batch-size: 1, 8180-2s
 
-if [ "x$m" = "xvgg-8180-1s" ]; then
+if [ "x$m" = "xvgg-n1-8180-1s" ]; then
   # vgg19_conv1_2, 7.0T
   ./script/run.sh -c -i64 -h224 -o64 -H224 -v0 -n1 --blk-i=4 --blk-o=4 --blk-t=29 --execution-mode=0xa040
   # vgg19_conv2_1, 7.3T
@@ -24,7 +24,7 @@ if [ "x$m" = "xvgg-8180-1s" ]; then
   ./script/run.sh -c -i512 -h14 -o512 -H14 -v0 -n1 --blk-i=8 --blk-o=4 --blk-t=25 --execution-mode=0xa000
 fi
 
-if [ "x$m" = "xvgg-8180-2s" ]; then
+if [ "x$m" = "xvgg-n1-8180-2s" ]; then
   # vgg19_conv1_2, 12.8T
   ./script/run.sh -c -i64 -h224 -o64 -H224 -v0 -n1 --blk-i=4 --blk-o=4 --blk-t=17 --execution-mode=0xa040
   # vgg19_conv2_1, 13.5T
@@ -47,7 +47,7 @@ fi
 
 # batch-size: 1, 8180-1s
 
-if [ "x$m" = "xresnet-8180-1s" ]; then
+if [ "x$m" = "xresnet-n1-8180-1s" ]; then
   # resnet50_res2a_branch2b, 5.2T
   ./script/run.sh -c -i64 -h56 -o64 -H56 -v0 -n1 --execution-mode=0xa061 --blk-i=4 --blk-o=2 --blk-t=26
   # resnet50_res3a_branch2b, 4.4T
@@ -58,7 +58,7 @@ if [ "x$m" = "xresnet-8180-1s" ]; then
   ./script/run.sh -c -i512 -h7 -o512 -H7 -v0 -n1 --execution-mode=0xa000 --blk-i=8 --blk-o=4 --blk-t=9
 fi
 
-if [ "x$m" = "xresnet-8180-1s" ]; then
+if [ "x$m" = "xresnet-n1-8180-2s" ]; then
   # resnet50_res2a_branch2b, 7.7T
   ./script/run.sh -c -i64 -h56 -o64 -H56 -v0 -n1 --execution-mode=0xa061 --blk-i=4 --blk-o=2 --blk-t=13
   # resnet50_res3a_branch2b, 6.0T
@@ -67,4 +67,15 @@ if [ "x$m" = "xresnet-8180-1s" ]; then
   ./script/run.sh -c -i256 -h14 -o256 -H14 -v0 -n1 --execution-mode=0xa000 --blk-i=8 --blk-o=1 --blk-t=9
   # resnet50_res5a_branch2b, 3.6T
   ./script/run.sh -c -i512 -h7 -o512 -H7 -v0 -n1 --execution-mode=0xa000 --blk-i=8 --blk-o=4 --blk-t=9
+fi
+
+if [ "x$m" = "xresnet-n64-8180-2s" ]; then
+  # resnet50_res2a_branch2b, 11.2T
+  ./script/run.sh -c -i64 -h56 -o64 -H56 -v0 -n64 --execution-mode=0xa040 --blk-i=4 --blk-o=4 --blk-t=28
+  # resnet50_res3a_branch2b, 11.2T
+  ./script/run.sh -c -i128 -h28 -o128 -H28 -v0 -n64 --execution-mode=0xa040 --blk-i=8 --blk-o=8 --blk-t=29
+  # resnet50_res4a_branch2b, 9.7T
+  ./script/run.sh -c -i256 -h14 -o256 -H14 -v0 -n64 --execution-mode=0xa061 --blk-i=8 --blk-o=4 --blk-t=23
+  # resnet50_res5a_branch2b, 6.7T
+  ./script/run.sh -c -i512 -h7 -o512 -H7 -v0 -n64 --execution-mode=0xa000 --blk-i=8 --blk-o=8 --blk-t=29
 fi
