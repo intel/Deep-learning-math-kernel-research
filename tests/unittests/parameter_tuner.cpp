e107cca39b097ea58ac9623e53c7f8ad0822cafa
caozhong <zhong.z.cao@intel.com>
2018-08-13 14:30:14 +0800

    Adding cases
    Signed-off-by: caozhong <zhong.z.cao@intel.com>

--- tests/unittests/parameter_tuner.cpp
+++ tests/unittests/parameter_tuner.cpp
@@ -7,10 +7,9 @@ class tuner_test_parameters {
 public:
   std::size_t l1_sz_, l2_sz_, l3_sz_;
   int core_num_;
-  int sockets;
+  int sockets_;
 
-  int i, h, w, o, H, W, n;
-  int pad_tl, pad_dr;
+  int i, h, o, H, n;
   // comparable facts
   int mode, blk_t, pat_o, blk_i, blk_o;
 };
@@ -26,9 +25,9 @@ public:
   virtual void SetUp() {
     auto p = testing::TestWithParam<tuner_test_parameters>::GetParam();
 
-    desc_.dims = { {p.n, p.o, p.h, p.w}, {p.o, p.i, p.K, p.K}, {p.n, p.o, p.H, p.W} };
+    desc_.dims = { {p.n, p.o, p.h, p.h}, {p.o, p.i, K, K}, {p.n, p.o, p.H, p.H} };
     desc_.formats = { euler::nChw16c, euler::OIhw16i16o, euler::nChw16c };
-    desc_.pads = {p.pad_tl, p.pad_tl, p.pad_dr, p.pad_dr};
+    desc_.pads = {1, 1, 1, 1};
     desc_.with_bias = false;
     desc_.algorithm = euler::CONV_WINOGRAD;
     desc_.with_relu = false;
@@ -46,7 +45,7 @@ public:
 using tuner_test_tile_5 = tuner_test<float, euler::ISA_SKX_AVX512, 16, 5, 3>;
 TEST_P(tuner_test_tile_5, tile_5) {
   auto p = testing::TestWithParam<tuner_test_parameters>::GetParam();
-  auto tile_oc4 = p_xc_->tile_blocking_oc4(p.core_num_);
+  auto tile_oc4 = p_xc_->tile_blocking_oc4(p.core_num_ * p.sockets_);
   auto blk_t = tile_oc4.first;
   auto pat_o = tile_oc4.second;
   auto blk_i = p_xc_->I2_num(p.l1_sz_, blk_t);
@@ -57,3 +56,18 @@ TEST_P(tuner_test_tile_5, tile_5) {
   EXPECT_EQ(blk_i, p.blk_i);
   EXPECT_EQ(blk_o, p.blk_o);
 }
+
+#define l1 (32 * 1024)
+#define l2 (1024 * 1024)
+#define l3 39424
+#define skx_8180  28
+using ttp = tuner_test_parameters;
+
+INSTANTIATE_TEST_CASE_P(tuner_test_tile_5, tuner_test_tile_5, 
+  ::testing::Values(
+    ttp {l1, l2, l3, skx_8180, 1, 64, 56, 64, 56, 1, 0xa061, 26, 2, 4, 2}
+    , ttp {l1, l2, l3, skx_8180, 1, 128, 28, 128, 28, 1, 0xa061, 15, 4, 8, 2}
+    , ttp {l1, l2, l3, skx_8180, 1, 256, 14, 256, 14, 1, 0xa000, 25, 4, 8, 8}
+    , ttp {l1, l2, l3, skx_8180, 1, 512, 7, 512, 7, 1, 0xa000, 5, 4, 8, 8}
+    , ttp {l1, l2, l3, skx_8180, 1, 512, 7, 512, 7, 1, 0xa000, 5, 4, 8, 8}
+));
