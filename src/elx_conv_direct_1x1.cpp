a87627f7bff4ce3cc6042d7dd1c3c43919841184
Yong Wu <yong.wu@intel.com>
2018-08-29 05:00:30 +0800

    gemm: kernel: move F into template pack
    Signed-off-by: Yong Wu <yong.wu@intel.com>

--- src/elx_conv_direct_1x1.cpp
+++ src/elx_conv_direct_1x1.cpp
@@ -211,10 +211,10 @@ void elx_conv_direct_1x1_t<Type, V, I>::bind_execute_functions()
   case T_:                                                                     \
     if (xopt_ == 0xa061) {                                                     \
       if (this->with_bias)                                                     \
-        *func = gemm_ker_cls_<0xCCC, 1, O_, T_, false, false, true, false,     \
+        *func = gemm_ker_cls_<1, 0xCCC, O_, T_, false, false, true, false,     \
             false>::execute;                                                   \
       else                                                                     \
-        *func = gemm_ker_cls_<0xCCC, 1, O_, T_, false, false, false, false,    \
+        *func = gemm_ker_cls_<1, 0xCCC, O_, T_, false, false, false, false,    \
             false>::execute;                                                   \
       break;                                                                   \
     }
