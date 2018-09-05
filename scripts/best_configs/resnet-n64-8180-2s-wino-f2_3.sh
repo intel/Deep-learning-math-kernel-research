5c775dbd73b58d7c8e17634f82064085970f1558
mingxiao <mingxiao.huang@intel.com>
2018-09-05 17:23:44 +0800

    change blk-t to flt-t in scripts/best_configs f(2,3) cases

--- scripts/best_configs/resnet-n64-8180-2s-wino-f2_3.sh
+++ scripts/best_configs/resnet-n64-8180-2s-wino-f2_3.sh
@@ -9,16 +9,16 @@
 source ./scripts/best_configs/common.sh $@
 
 # resnet50_res2a_branch2b, 11.8T  tflops:9.7289
-NSOCKETS=2 ./scripts/run.sh -c -i64 -h56 -o64 -H56 -n64 --tile-size=4 --execution-mode=0xa040 --blk-i=4 --blk-o=2 --blk-t=29 $COMMON
+NSOCKETS=2 ./scripts/run.sh -c -i64 -h56 -o64 -H56 -n64 --tile-size=4 --execution-mode=0xa040 --blk-i=4 --blk-o=2 --flt-t=29 $COMMON
 sleep 1
 # resnet50_res3a_branch2b, 11.9T  tflops:10.9736
-NSOCKETS=2 ./scripts/run.sh -c -i128 -h28 -o128 -H28 -n64 --tile-size=4 --execution-mode=0xa040 --blk-i=8 --blk-o=8 --blk-t=25 $COMMON
+NSOCKETS=2 ./scripts/run.sh -c -i128 -h28 -o128 -H28 -n64 --tile-size=4 --execution-mode=0xa040 --blk-i=8 --blk-o=8 --flt-t=25 $COMMON
 sleep 1
 # resnet50_res4a_branch2b, 10.0Ti  tflops:10.6828
-NSOCKETS=2 ./scripts/run.sh -c -i256 -h14 -o256 -H14 -n64 --tile-size=4 --execution-mode=0xa061 --blk-i=8 --blk-o=8 --blk-t=28 --pat-o=1 $COMMON
+NSOCKETS=2 ./scripts/run.sh -c -i256 -h14 -o256 -H14 -n64 --tile-size=4 --execution-mode=0xa061 --blk-i=8 --blk-o=8 --flt-t=28 --pat-o=1 $COMMON
 sleep 1
 # resnet50_res5a_branch2b, 6.7T  tflops:6.97863                                        0xa000
-NSOCKETS=2 ./scripts/run.sh -c -i512 -h7 -o512 -H7 -n64 --tile-size=4 --execution-mode=0xa201 --blk-i=8 --blk-o=16 --blk-t=25 $COMMON
+NSOCKETS=2 ./scripts/run.sh -c -i512 -h7 -o512 -H7 -n64 --tile-size=4 --execution-mode=0xa201 --blk-i=8 --blk-o=16 --flt-t=25 $COMMON
 # resnet50_res5a_branch2b, 6.7T  tflops:7.06438
-#NSOCKETS=2 ./scripts/run.sh -c -i512 -h7 -o512 -H7 -n64 --tile-size=4 --execution-mode=0xa0e1 --blk-i=8 --blk-o=8 --blk-t=30 pat-o=2 $COMMON
+#NSOCKETS=2 ./scripts/run.sh -c -i512 -h7 -o512 -H7 -n64 --tile-size=4 --execution-mode=0xa0e1 --blk-i=8 --blk-o=8 --flt-t=30 pat-o=2 $COMMON
 
--- scripts/best_configs/vgg-n1-8180-2s-wino-f2_3.sh
+++ scripts/best_configs/vgg-n1-8180-2s-wino-f2_3.sh
@@ -7,25 +7,25 @@
 source ./scripts/best_configs/common.sh $@
 
 # vgg19_conv1_2, 12.8T tflops=10.5969
-NSOCKETS=2 ./scripts/run.sh -c -i64 -h224 -o64 -H224 -n1 --blk-i=4 --blk-o=2 --blk-t=28 --tile-size=4 --execution-mode=0xa040 --output-as-blocked=true $COMMON
+NSOCKETS=2 ./scripts/run.sh -c -i64 -h224 -o64 -H224 -n1 --blk-i=4 --blk-o=2 --flt-t=28 --tile-size=4 --execution-mode=0xa040 --output-as-blocked=true $COMMON
 sleep 1
 # vgg19_conv2_1, 13.5T tflops=11.1854
-NSOCKETS=2 ./scripts/run.sh -c -i64 -h112 -o128 -H112 -n1 --blk-i=4 --blk-o=8 --blk-t=28 --tile-size=4 --execution-mode=0xa040 $COMMON
+NSOCKETS=2 ./scripts/run.sh -c -i64 -h112 -o128 -H112 -n1 --blk-i=4 --blk-o=8 --flt-t=28 --tile-size=4 --execution-mode=0xa040 $COMMON
 sleep 1
 # vgg19_conv2_2, 14.3T tflops=11.7138
-NSOCKETS=2 ./scripts/run.sh -c -i128 -h112 -o128 -H112 -n1 --blk-i=8 --blk-o=8 --blk-t=28 --tile-size=4 --execution-mode=0xa040 $COMMON
+NSOCKETS=2 ./scripts/run.sh -c -i128 -h112 -o128 -H112 -n1 --blk-i=8 --blk-o=8 --flt-t=28 --tile-size=4 --execution-mode=0xa040 $COMMON
 sleep 1
 # vgg19_conv3_1, 13.0T tflops=11.5115
-NSOCKETS=2 ./scripts/run.sh -c -i128 -h56 -o256 -H56 -n1 --blk-i=8 --blk-o=4 --blk-t=28 --pat-o=2 --tile-size=4 --execution-mode=0xa061 $COMMON
+NSOCKETS=2 ./scripts/run.sh -c -i128 -h56 -o256 -H56 -n1 --blk-i=8 --blk-o=4 --flt-t=28 --pat-o=2 --tile-size=4 --execution-mode=0xa061 $COMMON
 sleep 1
 # vgg19_conv3_2, 12.6T tflops=11.1579
-NSOCKETS=2 ./scripts/run.sh -c -i256 -h56 -o256 -H56 -n1 --blk-i=8 --blk-o=8 --blk-t=28 --pat-o=2 --tile-size=4 --execution-mode=0xa061 $COMMON
+NSOCKETS=2 ./scripts/run.sh -c -i256 -h56 -o256 -H56 -n1 --blk-i=8 --blk-o=8 --flt-t=28 --pat-o=2 --tile-size=4 --execution-mode=0xa061 $COMMON
 sleep 1
 # vgg19_conv4_1, 10.2T   tflops=10.2682
-NSOCKETS=2 ./scripts/run.sh -c -i256 -h28 -o512 -H28 -n1 --blk-i=8 --blk-o=16 --blk-t=28 --tile-size=4 --execution-mode=0xa000 --streaming-input=1 $COMMON
+NSOCKETS=2 ./scripts/run.sh -c -i256 -h28 -o512 -H28 -n1 --blk-i=8 --blk-o=16 --flt-t=28 --tile-size=4 --execution-mode=0xa000 --streaming-input=1 $COMMON
 sleep 1
 # vgg19_conv4_2, 10.5T tflops= 11.6569
-NSOCKETS=2 ./scripts/run.sh -c -i512 -h28 -o512 -H28 -n1 --blk-i=8 --blk-o=32 --blk-t=28 --tile-size=4 --execution-mode=0xa000 --streaming-input=1 $COMMON
+NSOCKETS=2 ./scripts/run.sh -c -i512 -h28 -o512 -H28 -n1 --blk-i=8 --blk-o=32 --flt-t=28 --tile-size=4 --execution-mode=0xa000 --streaming-input=1 $COMMON
 sleep 1
 # vgg19_conv5_1, 7.3T tflops=8.14171
-NSOCKETS=2 ./scripts/run.sh -c -i512 -h14 -o512 -H14 -n1 --blk-i=16 --blk-o=1 --blk-t=25 --tile-size=4 --execution-mode=0xa000 $COMMON
+NSOCKETS=2 ./scripts/run.sh -c -i512 -h14 -o512 -H14 -n1 --blk-i=16 --blk-o=1 --flt-t=25 --tile-size=4 --execution-mode=0xa000 $COMMON
