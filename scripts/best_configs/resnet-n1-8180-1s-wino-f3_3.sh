a45183b7fa42a30a6cca82963750f20b92433208
Yong Wu <yong.wu@intel.com>
2018-09-05 22:00:57 +0900

    scripts: resnets parameters tuning
    Signed-off-by: Yong Wu <yong.wu@intel.com>

--- scripts/best_configs/resnet-n1-8180-1s-wino-f3_3.sh
+++ scripts/best_configs/resnet-n1-8180-1s-wino-f3_3.sh
@@ -7,13 +7,13 @@
 source ./scripts/best_configs/common.sh $@
 
 # resnet50_res2a_branch2b, 5.2T
-NSOCKETS=1 ./scripts/run.sh -c -i64 -h56 -o64 -H56 -n1 --tile-size=5 --execution-mode=0xa040 --blk-i=4 --blk-o=4 --flt-t=13 --pat-o=1 --output-as-blocked=true $COMMON
+NSOCKETS=1 ./scripts/run.sh -c -i64 -h56 -o64 -H56 -n1 --tile-size=5 --execution-mode=0xa061 --blk-i=4 --blk-o=2 --flt-o=2 --flt-t=13 --pat-o=1 --output-as-blocked=true  $COMMON
 sleep 1
 # resnet50_res3a_branch2b, 4.4T
 NSOCKETS=1 ./scripts/run.sh -c -i128 -h28 -o128 -H28 -n1 --tile-size=5 --execution-mode=0xa061 --blk-i=8 --blk-o=2 --flt-t=15 --pat-o=4 --output-as-blocked=true $COMMON
 sleep 1
 # resnet50_res4a_branch2b, 4.2T
-NSOCKETS=1 ./scripts/run.sh -c -i256 -h14 -o256 -H14 -n1 --tile-size=5 --execution-mode=0xa000 --blk-i=8 --blk-o=4 --flt-t=25 $COMMON
+NSOCKETS=1 ./scripts/run.sh -c -i256 -h14 -o256 -H14 -n1 --tile-size=5 --execution-mode=0xa000 --blk-i=2 --blk-o=2 --flt-o=2 --flt-t=13 $COMMON
 sleep 1
 # resnet50_res5a_branch2b, 2.5T
-NSOCKETS=1 ./scripts/run.sh -c -i512 -h7 -o512 -H7 -n1 --tile-size=5 --execution-mode=0xa000 --blk-i=8 --blk-o=4 --flt-t=9 $COMMON
+NSOCKETS=1 ./scripts/run.sh -c -i512 -h7 -o512 -H7 -n1 --tile-size=5 --execution-mode=0xa000 --blk-i=4 --blk-o=4 --flt-o=2 --flt-t=9 $COMMON
