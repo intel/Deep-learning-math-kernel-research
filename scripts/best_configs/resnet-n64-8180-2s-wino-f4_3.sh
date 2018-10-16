4c71acd34783b452a61c7d7799be83d591afcc59
mingxiao <mingxiao.huang@intel.com>
2018-10-16 16:03:42 +0800

    modify test config since 0xa060 execution mode is not supported any more

--- scripts/best_configs/resnet-n64-8180-2s-wino-f4_3.sh
+++ scripts/best_configs/resnet-n64-8180-2s-wino-f4_3.sh
@@ -15,7 +15,7 @@ sleep 1
 NSOCKETS=2 ./scripts/run.sh -c -i128 -h28 -o128 -H28 -n64 --tile-size=6 --execution-mode=0xa061 --blk-i=8 --blk-o=8 --flt-t=28 $COMMON
 sleep 1
 # resnet50_res4a_branch2b, 10.0T
-NSOCKETS=2 ./scripts/run.sh -c -i256 -h14 -o256 -H14 -n64 --tile-size=6 --execution-mode=0xa060 --blk-i=8 --blk-o=4 --flt-t=25 --pat-o=4 $COMMON
+NSOCKETS=2 ./scripts/run.sh -c -i256 -h14 -o256 -H14 -n64 --tile-size=6 --execution-mode=0xa061 --blk-i=8 --blk-o=4 --flt-t=19 --pat-o=4 $COMMON
 sleep 1
 # resnet50_res5a_branch2b, 6.7T
 NSOCKETS=2 ./scripts/run.sh -c -i512 -h7 -o512 -H7 -n64 --tile-size=6 --execution-mode=0xa000 --blk-i=8 --blk-o=8 --flt-t=29 $COMMON
--- scripts/best_configs/vgg-n1-8180-2s-wino-f4_3.sh
+++ scripts/best_configs/vgg-n1-8180-2s-wino-f4_3.sh
@@ -10,7 +10,7 @@ source ./scripts/best_configs/common.sh $@
 NSOCKETS=2 ./scripts/run.sh -c -i64 -h224 -o64 -H224 -n1 --blk-i=4 --blk-o=4 --flt-t=28 --tile-size=6 --execution-mode=0xa061 --output-as-blocked=true $COMMON
 sleep 1
 # vgg19_conv2_1, 13.5T
-NSOCKETS=2 ./scripts/run.sh -c -i64 -h112 -o128 -H112 -n1 --blk-i=4 --blk-o=4 --flt-t=14 --tile-size=6 --pat-o=2 --execution-mode=0xa060 $COMMON
+NSOCKETS=2 ./scripts/run.sh -c -i64 -h112 -o128 -H112 -n1 --blk-i=4 --blk-o=4 --flt-t=14 --tile-size=6 --pat-o=2 --execution-mode=0xa061 $COMMON
 sleep 1
 # vgg19_conv2_2, 14.3T
 NSOCKETS=2 ./scripts/run.sh -c -i128 -h112 -o128 -H112 -n1 --blk-i=8 --blk-o=8 --flt-t=14 --tile-size=6 --execution-mode=0xa061 $COMMON
@@ -19,7 +19,7 @@ sleep 1
 NSOCKETS=2 ./scripts/run.sh -c -i128 -h56 -o256 -H56 -n1 --blk-i=8 --blk-o=4 --flt-t=14 --pat-o=4 --tile-size=6 --execution-mode=0xa061 $COMMON
 sleep 1
 # vgg19_conv3_2, 12.6T
-NSOCKETS=2 ./scripts/run.sh -c -i256 -h56 -o256 -H56 -n1 --blk-i=8 --blk-o=4 --flt-t=14 --pat-o=4 --tile-size=6 --execution-mode=0xa060 $COMMON
+NSOCKETS=2 ./scripts/run.sh -c -i256 -h56 -o256 -H56 -n1 --blk-i=8 --blk-o=4 --flt-t=14 --pat-o=4 --tile-size=6 --execution-mode=0xa061 $COMMON
 sleep 1
 # vgg19_conv4_1, 10.2T
 NSOCKETS=2 ./scripts/run.sh -c -i256 -h28 -o512 -H28 -n1 --blk-i=8 --blk-o=4 --flt-t=13 --tile-size=6 --execution-mode=0xa000 --streaming-input=1 $COMMON
