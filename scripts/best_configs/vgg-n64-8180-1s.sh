5b437b269dd516593a8b5adbd3b37e032d521b2e
Yong Wu <yong.wu@intel.com>
2018-10-21 22:30:54 +0900

    test: best_configs: vgg-n64-8180-1s
    Signed-off-by: Yong Wu <yong.wu@intel.com>

---
+++ scripts/best_configs/vgg-n64-8180-1s.sh
@@ -0,0 +1,32 @@
+#/bin/bash
+
+# VGG
+# batch-size: 64
+# SKX 8180 2S
+
+source ./scripts/best_configs/common.sh $@
+
+
+# vgg19_conv1_2
+NSOCKETS=1 ./scripts/run.sh -c -i64 -h224 -o64 -H224 -n64 --blk-i=4 --blk-o=1 --flt-o=2 --flt-t=14 --tile-size=6 --execution-mode=0xa061 --pat-o=2 --output-as-blocked=true -v0 $COMMON
+sleep 1
+# vgg19_conv2_1
+NSOCKETS=1 ./scripts/run.sh -c -i64 -h112 -o128 -H112 -n64 --tile-size=6 --blk-i=4 --blk-o=1 --flt-o=2 --flt-t=14 --execution-mode=0xa061 --pat-o=4 $COMMON
+sleep 1
+# vgg19_conv2_2
+NSOCKETS=1 ./scripts/run.sh -c -i128 -h112 -o128 -H112 -n64 --tile-size=6 --blk-i=8 --blk-o=1 --flt-t=28 --execution-mode=0xa061 --pat-o=8 $COMMON
+sleep 1
+# vgg19_conv3_1
+NSOCKETS=1 ./scripts/run.sh -c -i128 -h56 -o256 -H56 -n64 --tile-size=6 --blk-i=8 --blk-o=1 --flt-o=2 --flt-t=14 --pat-o=8 --execution-mode=0xa061 $COMMON
+sleep 1
+# vgg19_conv3_2
+NSOCKETS=1 ./scripts/run.sh -c -i256 -h56 -o256 -H56 -n64 --tile-size=6 --blk-i=8 --blk-o=2 --flt-o=2 --flt-t=14 --pat-o=4 --execution-mode=0xa061 $COMMON
+sleep 1
+# vgg19_conv4_1
+NSOCKETS=1 ./scripts/run.sh -c -i256 -h28 -o512 -H28 -n64 --tile-size=6 --blk-i=8 --blk-o=8 --flt-t=31 --execution-mode=0xa000 --streaming-input=2 $COMMON
+sleep 1
+# vgg19_conv4_2
+NSOCKETS=1 ./scripts/run.sh -c -i512 -h28 -o512 -H28 -n64 --tile-size=6 --blk-i=8 --blk-o=4 --flt-t=31 --execution-mode=0xa000 --streaming-input=2 $COMMON
+sleep 1
+# vgg19_conv5_1
+NSOCKETS=1 ./scripts/run.sh -c -i512 -h14 -o512 -H14 -n64 --tile-size=7 --blk-i=8 --blk-o=4 --flt-t=28 --execution-mode=0xa000 --streaming-input=2 $COMMON
