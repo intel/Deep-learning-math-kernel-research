e3c2ed203bbec894a69737f83cdedfb7c20f3811
Yong Wu <yong.wu@intel.com>
2018-11-06 10:54:27 +0900

    test: best_configs: update vgg-n1-8180-1s
    Signed-off-by: Yong Wu <yong.wu@intel.com>

--- scripts/best_configs/vgg-n1-8180-1s.sh
+++ scripts/best_configs/vgg-n1-8180-1s.sh
@@ -24,7 +24,7 @@ NSOCKETS=1 ./scripts/run.sh -c -i128 -h56 -o256 -H56 -n1 --blk-i=2 --blk-o=2 --f
 
 sleep 1
 # vgg19_conv3_2,
-NSOCKETS=1 ./scripts/run.sh -c -i256 -h56 -o256 -H56 -n1 --blk-i=8 --blk-o=2 --flt-o=1 --flt-t=28 --pat-i=2 --pat-o=8 --tile-size=6 --execution-mode=0xa071 $COMMON
+NSOCKETS=1 ./scripts/run.sh -c -i256 -h56 -o256 -H56 -n1 --blk-i=8 --blk-o=2 --flt-o=1 --flt-t=28 --pat-i=2 --pat-o=8 --tile-size=5 --execution-mode=0xa071 $COMMON
 
 sleep 1
 # vgg19_conv4_1, 6.6T
