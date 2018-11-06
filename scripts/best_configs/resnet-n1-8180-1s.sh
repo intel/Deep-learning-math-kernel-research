36595a263c0cdfdf428efd09fea6e44533f293a6
Yong Wu <yong.wu@intel.com>
2018-11-06 23:30:14 +0900

    test: best_configs: update vgg and resnet
    Signed-off-by: Yong Wu <yong.wu@intel.com>

--- scripts/best_configs/resnet-n1-8180-1s.sh
+++ scripts/best_configs/resnet-n1-8180-1s.sh
@@ -13,7 +13,7 @@ sleep 1
 NSOCKETS=1 ./scripts/run.sh -c -i128 -h28 -o128 -H28 -n1 --tile-size=4 --execution-mode=0xa061 --blk-i=8 --blk-o=2 --flt-o=2 --flt-t=14 --pat-o=2 $COMMON
 sleep 1
 # resnet50_res4a_branch2b, 4.2T
-NSOCKETS=1 ./scripts/run.sh -c -i256 -h14 -o256 -H14 -n1 --tile-size=5 --execution-mode=0xa000 --blk-i=8 --blk-o=8 --flt-o=1 --flt-t=25 $COMMON
+NSOCKETS=1 ./scripts/run.sh -c -i256 -h14 -o256 -H14 -n1 --tile-size=4 --execution-mode=0xa000 --blk-i=8 --blk-o=1 --flt-o=2 --flt-t=13 $COMMON
 sleep 1
 # resnet50_res5a_branch2b, 2.5T
-NSOCKETS=1 ./scripts/run.sh -c -i512 -h7 -o512 -H7 -n1 --tile-size=4 --execution-mode=0xa000 --blk-i=4 --blk-o=4 --flt-o=1 --flt-t=16 $COMMON
+NSOCKETS=1 ./scripts/run.sh -c -i512 -h7 -o512 -H7 -n1 --tile-size=4 --execution-mode=0xa000 --blk-i=16 --blk-o=1 --flt-o=2 --flt-t=8 $COMMON
