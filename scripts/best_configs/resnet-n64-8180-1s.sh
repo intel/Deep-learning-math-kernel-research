7db99adf2d38a53ea435bae5a22b59148f35de8d
Yong Wu <yong.wu@intel.com>
2018-11-08 01:29:04 +0900

    tests: best_configs: update resnet-n1-8180-1s
    Signed-off-by: Yong Wu <yong.wu@intel.com>

--- scripts/best_configs/resnet-n64-8180-1s.sh
+++ scripts/best_configs/resnet-n64-8180-1s.sh
@@ -16,4 +16,4 @@ sleep 1
 NSOCKETS=1 ./scripts/run.sh -c -i256 -h14 -o256 -H14 -n64 --tile-size=7 --execution-mode=0xa073 --blk-i=4 --blk-o=1 --flt-o=2 --flt-t=14 --pat-i=4 --pat-o=8 $COMMON
 sleep 1
 # resnet50_res5a_branch2b, 2.5T
-NSOCKETS=1 ./scripts/run.sh -c -i512 -h7 -o512 -H7 -n64 --tile-size=6 --execution-mode=0xa033 --blk-i=8 --blk-o=2 --flt-t=29 --pat-i=1 --pat-o=2 $COMMON
+NSOCKETS=1 ./scripts/run.sh -c -i512 -h7 -o512 -H7 -n64 --tile-size=6 --execution-mode=0xa033 --blk-i=8 --blk-o=8 --flt-t=26 --pat-i=1 --pat-o=1 $COMMON
