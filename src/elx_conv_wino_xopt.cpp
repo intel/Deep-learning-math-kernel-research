8bd276ea6bdc33eaaea256de6c608fe5821e59d4
fengyuan <feng1.yuan@intel.com>
2018-11-28 16:19:26 +0800

    tinput: int8: bugfix a133 precision issue caused by lack of synchronization
    Signed-off-by: fengyuan <feng1.yuan@intel.com>

--- src/elx_conv_wino_xopt.cpp
+++ src/elx_conv_wino_xopt.cpp
@@ -473,6 +473,7 @@ void Instance_elx_conv_wino_t::__execute_a133(
     iter_each(_oc4, this->oc4) {
       if (_ic4 != last_ic4) {
         trans_input(tinput_, &md3(ainput, 0, _ic4, 0));
+#pragma omp barrier
         trans_input_quantization(tinput_u8_, tinput_qt_scale_, tinput_max_abs_, tinput_);
         last_ic4 = _ic4;
       }
