1487dc84c0cc887dcf77112c9a58a62d39dd8d0a
Yong Wu <yong.wu@intel.com>
2019-01-05 20:38:26 +0800

    conv: direct: shape/format check and optimize a060 khs/kwe compute
    Signed-off-by: Yong Wu <yong.wu@intel.com>

--- src/elx_conv_direct.cpp
+++ src/elx_conv_direct.cpp
@@ -46,12 +46,19 @@ Instance_elx_conv_direct_t::elx_conv_direct_t(eld_conv_t<UserTypes> &dc)
     if (this->T <= this->lp || this->Tr <= this->rp) {
       el_error("Unimplemented T: (T,Tr) must greater than (lp,rp)");
     }
-
-    // kh,kw=odd, lp=rp, tp=bp, ih=oh*hs, iw=ow*ws
-    if (this->kh % 2 != 1 || this->kw % 2 != 1
-        || this->lp != this->rp || this->tp != this->bp
-        || this->ih != this->oh * this->hs || this->iw != this->ow * this->ws) {
-      el_error("a060|d060: shape not supported");
+    bool shape_ok = xopt_ == 0xa060
+      && estl::any_of(this->kh, 3, 5, 7)
+      && estl::any_of(this->kw, 3, 5, 7)
+      && this->ih == this->oh && this->iw == this->ow
+      && this->hs == 1 && this->ws == 1
+      && this->lp == (this->kw / 2) && (this->tp == this->kh / 2);
+    if (!shape_ok) {
+        el_error("direct: a060: shape not supported");
+    }
+    bool format_ok = estl::any_of(this->input_fmt, nchw, nChw16c)
+      && this->output_fmt == nChw16c && this->weights_fmt == OIhw16i16o;
+    if (!format_ok) {
+      el_error("direct: format not supported");
     }
   }
 
@@ -279,10 +286,10 @@ Instance_elx_conv_direct_t::conv_a060(OutputType *output,
   int Tz = _wt == this->wt - 1 ? this->Tr : this->T;
   auto ker_conv = _wt == this->wt - 1 ? ker_conv_Tr_ : ker_conv_;
 
-  int khs = std::max(0, this->tp - this->hs * _ht);
-  int khe = std::min(this->kh, this->ih + this->tp - this->hs * _ht);
-  int kws = std::max(0, this->lp - this->ws * _wt * this->T);
-  int kwe = std::min(this->kw, this->iw + this->lp - this->ws * (_wt * this->T + Tz - 1));
+  int khs = std::max(0, this->tp - _ht);
+  int khe = std::min(this->kh, this->ih + this->tp - _ht);
+  int kws = _wt == 0 ? this->lp : 0;
+  int kwe = _wt == this->wt - 1 ? this->kw - this->lp : this->kw;
   assert(this->T > this->lp && this->Tr > this->rp);
 
   iter_each(_oc3, this->oc3) {
@@ -311,11 +318,13 @@ void Instance_elx_conv_direct_t::gemm_d060_blocked_input(OutputType *output, Inp
 
   const int AKH = this->kh / 2;
   const int AKW = this->kw / 2;
+  int Tz = _wt == this->wt - 1 ? this->Tr : this->T;
 
-  int khs = _ht == 0 ? 1 : 0;
-  int khe = _ht == this->ht - 1 ? this->kh - 1 : this->kh;
-  int kws = _wt == 0 ? 1 : 0;
-  int kwe = _wt == this->wt - 1 ? this->kw - 1 : this->kw;
+  int khs = std::max(0, this->tp - this->hs * _ht);
+  int khe = std::min(this->kh, this->ih + this->tp - this->hs * _ht);
+  int kws = std::max(0, this->lp - this->ws * _wt * this->T);
+  int kwe = std::min(this->kw, this->iw + this->lp - this->ws * (_wt * this->T + Tz - 1));
+
   assert(this->T > this->lp);
   assert(this->Tr > this->rp);
 
