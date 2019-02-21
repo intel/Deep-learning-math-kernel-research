9010a9bd1cbc2d53292bf2c0470144286efc8ffd
Yong Wu <yong.wu@intel.com>
2019-02-21 18:27:03 +0900

    tests: scripts: update direct conv configs
    Signed-off-by: Yong Wu <yong.wu@intel.com>

--- scripts/best_configs/vgg-n1-8180-1s-direct.sh
+++ scripts/best_configs/vgg-n1-8180-1s-direct.sh
@@ -15,24 +15,24 @@ NSOCKETS=1 ./scripts/run.sh -c -i64 -h112 -o128 -H112 -n1 --blk-i=4 --blk-o=2 --
 
 sleep 1
 # vgg19_conv2_2,
-NSOCKETS=1 ./scripts/run.sh -c -i128 -h112 -o128 -H112 -n1 --blk-i=8 --blk-o=4 --flt-o=2 --flt-t=14 -adirect --execution-mode=0xa060 --pat-o=1 $COMMON
+NSOCKETS=1 ./scripts/run.sh -c -i128 -h112 -o128 -H112 -n1 --blk-i=4 --blk-o=2 --flt-o=2 --flt-t=14 -adirect --execution-mode=0xa060 --pat-i=2 --pat-o=1 $COMMON
 
 sleep 1
 # vgg19_conv3_1,
-NSOCKETS=1 ./scripts/run.sh -c -i128 -h56 -o256 -H56 -n1 --blk-i=8 --blk-o=1 --flt-o=2 --flt-t=14 --pat-i=1 --pat-o=1 -adirect --execution-mode=0xa060 $COMMON
+NSOCKETS=1 ./scripts/run.sh -c -i128 -h56 -o256 -H56 -n1 --blk-i=4 --blk-o=2 --flt-o=2 --flt-t=14 --pat-i=2 --pat-o=1 -adirect --execution-mode=0xa060 $COMMON
 
 sleep 1
 # vgg19_conv3_2,
-NSOCKETS=1 ./scripts/run.sh -c -i256 -h56 -o256 -H56 -n1 --blk-i=8 --blk-o=1 --flt-o=2 --flt-t=14 --pat-i=2 --pat-o=2 -adirect --execution-mode=0xa060 $COMMON
+NSOCKETS=1 ./scripts/run.sh -c -i256 -h56 -o256 -H56 -n1 --blk-i=4 --blk-o=2 --flt-o=2 --flt-t=14 --pat-i=4 --pat-o=1 -adirect --execution-mode=0xa060 $COMMON
 
 sleep 1
 # vgg19_conv4_1, 6.6T
-NSOCKETS=1 ./scripts/run.sh -c -i256 -h28 -o512 -H28 -n1 --blk-i=8 --blk-o=2 --flt-o=2 --flt-t=14 -adirect --execution-mode=0xa060 --pat-i=2 --pat-o=1 $COMMON
+NSOCKETS=1 ./scripts/run.sh -c -i256 -h28 -o512 -H28 -n1 --blk-i=2 --blk-o=1 --flt-o=2 --flt-t=14 -adirect --execution-mode=0xa060 --pat-i=8 --pat-o=1 $COMMON
 
 sleep 1
 # vgg19_conv4_2, 6.4T
-NSOCKETS=1 ./scripts/run.sh -c -i512 -h28 -o512 -H28 -n1 --blk-i=8 --blk-o=1 --flt-o=2 --flt-t=14 -adirect --execution-mode=0xa060 --pat-i=4 --pat-o=1 $COMMON
+NSOCKETS=1 ./scripts/run.sh -c -i512 -h28 -o512 -H28 -n1 --blk-i=4 --blk-o=1 --flt-o=2 --flt-t=14 -adirect --execution-mode=0xa060 --pat-i=8 --pat-o=1 $COMMON
 
 sleep 1
 # vgg19_conv5_1, 5.2T
-NSOCKETS=1 ./scripts/run.sh -c -i512 -h14 -o512 -H14 -n1 --blk-i=8 --blk-o=1 --flt-o=2 --flt-t=14 -adirect --execution-mode=0xa060 --pat-i=2 --pat-o=1 $COMMON
+NSOCKETS=1 ./scripts/run.sh -c -i512 -h14 -o512 -H14 -n1 --blk-i=2 --blk-o=1 --flt-o=1 --flt-t=14 -adirect --execution-mode=0xa060 --pat-i=16 --pat-o=1 $COMMON
