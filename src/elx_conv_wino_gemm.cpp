a351a7f7c88ed134c38b4d0d18a984704d9b4cbd
Yong Wu <yong.wu@intel.com>
2019-03-04 23:01:28 +0900

    conv: wino: gemm: simplify gemm driver for a033
    Signed-off-by: Yong Wu <yong.wu@intel.com>

--- src/elx_conv_wino_gemm.cpp
+++ src/elx_conv_wino_gemm.cpp
@@ -178,7 +178,8 @@ void elx_conv_wino_gemm_t<GarrayTypes, A, V, I>::execute(
     ToutputType *toutput, TinputType *tinput, TweightsType *tweights, int _ic4)
 {
   int ithr = omp_get_thread_num();
-  thread_parallel_for<5, 2>(mthr_, ithr, [&](int _wA, int _hA, int _ic3, int _oc3, int _t2) {
+  thread_parallel_for<5, 2>(mthr_, ithr, [&](int _wA, int _hA,
+                                             int _ic3, int _oc3, int _t2) {
     MD2(TinputType, atinput2, tinput, xc->t2, A * A * xc->T * xc->ic3 * xc->I2 * V);
     MD2(ToutputType, atoutput2, toutput, xc->t2, A * A * xc->T * xc->oc3 * xc->O2 * V);
     MD5(TweightsType, atweights, tweights, xc->oc3, xc->ic3, A, A, xc->O2 * xc->I2 * V * V);
@@ -206,7 +207,8 @@ void elx_conv_wino_gemm_t<GarrayTypes, A, V, I>::execute_na(
     ToutputType *toutput, TinputType *tinput, TweightsType *tweights, int _ic4)
 {
   int ithr = omp_get_thread_num();
-  thread_parallel_for<4>(mthr_, ithr, [&](int _wA, int _hA, int _oc3, int _t2) {
+  thread_parallel_for<5, 3>(mthr_, ithr, [&](int _wA, int _hA,
+                                             int _oc3, int _ic3, int _t2) {
     MD2(TinputType, atinput2, tinput, xc->t2, A * A * xc->T * xc->ic3 * xc->I2 * V);
     MD2(ToutputType, atoutput2, toutput, xc->t2, A * A * xc->T * xc->oc3 * xc->O2 * V);
     MD5(TweightsType, atweights, tweights, xc->oc3, xc->ic3, A, A, xc->O2 * xc->I2 * V * V);
@@ -215,30 +217,18 @@ void elx_conv_wino_gemm_t<GarrayTypes, A, V, I>::execute_na(
     auto ker_gemm = (_t2 == xc->t2 - 1) ? ker_gemm0_ : ker_gemm_;
     MD6(TinputType, atinput6, &md2(atinput2, _t2, 0), A, A, xc->ic3, xc->I2, Tz, V);
     MD6(ToutputType, atoutput6, &md2(atoutput2, _t2, 0), A, A, xc->oc3, xc->O2, Tz, V);
-    bool last_ic4 = _ic4 == xc->ic4 - 1;
-    int ic3 = last_ic4 ? xc->ic3 - 1 : xc->ic3;
 
-    iter_each (_ic3, ic3) {
-      int attr = _ic3 == 0 ?
-          set_attr(attr_, r_output_idx) : attr_;
-      ker_gemm(*xc,
-          &md6(atoutput6, _wA, _hA, _oc3, 0, 0, 0),
-          &md6(atinput6, _wA, _hA, _ic3, 0, 0, 0),
-          &md5(atweights, _oc3, _ic3, _wA, _hA, 0),
-          nullptr, attr, 0, nullptr, nullptr, nullptr);
-    }
-    if (last_ic4) {
-      int attr = xc->ic3 == 1 ?
-          set_attr(attr_, r_output_idx) : attr_;
-      if (xc->Ir != V * xc->Vx)
-        attr = set_attr(attr, has_Ir_idx);
-      ker_gemm(*xc,
-          &md6(atoutput6, _wA, _hA, _oc3, 0, 0, 0),
-          &md6(atinput6, _wA, _hA, xc->ic3 - 1, 0, 0, 0),
-          &md5(atweights, _oc3, xc->ic3 - 1, _wA, _hA, 0),
-          nullptr, attr, 0, nullptr, nullptr, nullptr);
-    }
-  }, A, A, xc->oc3, xc->t2);
+    int attr = attr_;
+    if (_ic3 == 0)
+      attr = set_attr(attr, r_output_idx);
+    if (xc->Ir != V * xc->Vx && _ic4 == xc->ic4 - 1 && _ic3 == xc->ic3 - 1)
+      attr = set_attr(attr, has_Ir_idx);
+
+    ker_gemm(*xc, &md6(atoutput6, _wA, _hA, _oc3, 0, 0, 0),
+             &md6(atinput6, _wA, _hA, _ic3, 0, 0, 0),
+             &md5(atweights, _oc3, _ic3, _wA, _hA, 0), nullptr, attr, 0,
+             nullptr, nullptr, nullptr);
+  }, A, A, xc->oc3, xc->ic3, xc->t2);
 }
 
 } // namespace euler
