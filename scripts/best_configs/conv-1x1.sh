273a965993229fbb1d2090f8908aa021cb448d65
hongzhen <hongzhen.liu@intel.com>
2019-03-07 11:02:21 +0800

    scripts: update 1x1 configs
    Signed-off-by: hongzhen <hongzhen.liu@intel.com>

--- scripts/best_configs/conv-1x1.sh
+++ scripts/best_configs/conv-1x1.sh
@@ -62,11 +62,11 @@ NSOCKETS=1 ./scripts/run.sh -c -n1 -i256 -o128 -h28 -w28 -H28 -W28 -k1 -K1 -p0 -
 # resnet_50_sparse:res4a_branch1
 NSOCKETS=1 ./scripts/run.sh -c -n1 -i512 -o1024 -h14 -w14 -H14 -W14 -k1 -K1 -p0 -P0 -s1 -S1 -b1 -adirect_1x1 --blk-i=16 --flt-o=2 --flt-t=14 --execution-mode=0xc060 --pat-o=1 --pat-i=2 $COMMON
 # resnet_50_sparse:res4a_branch2a
-NSOCKETS=1 ./scripts/run.sh -c -n1 -i512 -o256 -h14 -w14 -H14 -W14 -k1 -K1 -p0 -P0 -s1 -S1 -b1 -adirect_1x1 --blk-i=32 --flt-o=8 --flt-t=7 --execution-mode=0xc060 --pat-o=2 $COMMON
+NSOCKETS=1 ./scripts/run.sh -c -n1 -i512 -o256 -h14 -w14 -H14 -W14 -k1 -K1 -p0 -P0 -s1 -S1 -b1 -adirect_1x1 --blk-i=32 --flt-o=2 --flt-t=14 --execution-mode=0xc060 --pat-o=2 $COMMON
 # resnet_50_sparse:res5a_branch1
-NSOCKETS=1 ./scripts/run.sh -c -n1 -i1024 -o2048 -h7 -w7 -H7 -W7 -k1 -K1 -p0 -P0 -s1 -S1 -b1 -adirect_1x1 --blk-i=64 --flt-o=2 --flt-t=10 --execution-mode=0xc060 $COMMON
+NSOCKETS=1 ./scripts/run.sh -c -n1 -i1024 -o2048 -h7 -w7 -H7 -W7 -k1 -K1 -p0 -P0 -s1 -S1 -b1 -adirect_1x1 --blk-i=8 --flt-o=2 --flt-t=14 --execution-mode=0xc060 --pat-o=2 --pat-i=8 $COMMON
 # resnet_50_sparse:res5a_branch2a
-NSOCKETS=1 ./scripts/run.sh -c -n1 -i1024 -o512 -h7 -w7 -H7 -W7 -k1 -K1 -p0 -P0 -s1 -S1 -b1 -adirect_1x1 --blk-i=64 --flt-o=4 --flt-t=7 --execution-mode=0xc060  $COMMON
+NSOCKETS=1 ./scripts/run.sh -c -n1 -i1024 -o512 -h7 -w7 -H7 -W7 -k1 -K1 -p0 -P0 -s1 -S1 -b1 -adirect_1x1 --blk-i=64 --flt-o=2 --flt-t=14 --execution-mode=0xc060 $COMMON
 
 
 # bs=64, stride=1, blocked
