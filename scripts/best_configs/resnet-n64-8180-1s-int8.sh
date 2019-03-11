d7c62b8310e66b9b43dc22a9d2c0b61b20a39a9e
Yong Wu <yong.wu@intel.com>
2019-03-11 20:28:26 +0900

    tests: scripts: int8: update vgg/resnet configs w.r.t new V blocking
    Signed-off-by: Yong Wu <yong.wu@intel.com>

--- scripts/best_configs/resnet-n64-8180-1s-int8.sh
+++ scripts/best_configs/resnet-n64-8180-1s-int8.sh
@@ -7,10 +7,10 @@
 source ./scripts/best_configs/common.sh $@
 
 # resnet50_res2a_branch2b 6.75T
-NSOCKETS=1 ./scripts/run.sh -c -i64 -h56 -o64 -H56 -n64 --tile-size=6 --execution-mode=0xa161 --blk-i=1 --blk-o=2 --flt-o=2 --flt-t=14 --pat-o=1 --output-as-blocked=true $COMMON
+NSOCKETS=1 ./scripts/run.sh -c -i64 -h56 -o64 -H56 -n64 --tile-size=6 --execution-mode=0xa161 --blk-i=4 --blk-o=2 --flt-o=2 --flt-t=14 --pat-o=1 --output-as-blocked=true $COMMON
 # resnet50_res3a_branch2b 5.62T
-NSOCKETS=1 ./scripts/run.sh -c -i128 -h28 -o128 -H28 -n64 --tile-size=6 --execution-mode=0xa161 --blk-i=2 --blk-o=2 --flt-o=2 --flt-t=14 --pat-o=2 $COMMON
+NSOCKETS=1 ./scripts/run.sh -c -i128 -h28 -o128 -H28 -n64 --tile-size=6 --execution-mode=0xa161 --blk-i=8 --blk-o=2 --flt-o=2 --flt-t=14 --pat-o=2 $COMMON
 # resnet50_res4a_branch2b 4.95T
-NSOCKETS=1 ./scripts/run.sh -c -i256 -h14 -o256 -H14 -n64 --tile-size=6 --execution-mode=0xa161 --blk-i=4 --blk-o=2 --flt-o=2 --flt-t=14 --pat-o=4 $COMMON
+NSOCKETS=1 ./scripts/run.sh -c -i256 -h14 -o256 -H14 -n64 --tile-size=6 --execution-mode=0xa161 --blk-i=16 --blk-o=2 --flt-o=2 --flt-t=14 --pat-o=4 $COMMON
 # resnet50_res5a_branch2b 3.19T
-NSOCKETS=1 ./scripts/run.sh -c -i512 -h7 -o512 -H7 -n64 --tile-size=6 --execution-mode=0xa161 --blk-i=8 --blk-o=2 --flt-o=2 --flt-t=13 --pat-o=8 $COMMON
+NSOCKETS=1 ./scripts/run.sh -c -i512 -h7 -o512 -H7 -n64 --tile-size=6 --execution-mode=0xa161 --blk-i=32 --blk-o=2 --flt-o=2 --flt-t=13 --pat-o=8 $COMMON
