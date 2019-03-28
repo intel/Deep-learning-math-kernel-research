87b8987ec7b1801ce5bded41ba335580ba1a0cb9
hongzhen <hongzhen.liu@intel.com>
2019-03-28 21:26:56 +0800

    cpuid: check isa with __cpuid and __cpuid_count
    Signed-off-by: hongzhen <hongzhen.liu@intel.com>

--- src/el_isa.hpp
+++ src/el_isa.hpp
@@ -1,6 +1,14 @@
 #pragma once
 
+#include <cpuid.h>
 #include <immintrin.h>
+
+#define SSE42      0x00100000 // ecx[bit 20]
+#define AVX        0x10000000 // ecx[bit 28]
+#define AVX2       0x00000020 // ebx[bit 5]
+#define AVX512F    0x00010000 // ebx[bit 16]
+#define AVX512BW   0x40000000 // ebx[bit 30]
+#define AVX512VNNI 0x00000800 // ecx[bit 11]
 
 enum {
   isa_undef = 0,
@@ -12,33 +20,41 @@ enum {
   avx512_core_vnni
 };
 
+struct cpuid_regs {
+  unsigned int eax;
+  unsigned int ebx;
+  unsigned int ecx;
+  unsigned int edx;
+};
+
+static cpuid_regs regs0, regs1;
+static void __attribute__ ((constructor)) get_cpuinfo(void) {
+  __cpuid(0x1, regs0.eax, regs0.ebx, regs0.ecx, regs0.edx);
+  __cpuid_count(0x7, 0, regs1.eax, regs1.ebx, regs1.ecx, regs1.edx);
+}
+
 static inline bool cpu_has(int isa)
 {
-#if __ICC_COMPILER
   switch (isa) {
   case sse42:
-    return _may_i_use_cpu_feature(_FEATURE_SSE4_2);
+    return !!(regs0.ecx & SSE42);
   case avx:
-     return _may_i_use_cpu_feature(_FEATURE_AVX);
+    return !!(regs0.ecx & AVX);
   case avx2:
-    return _may_i_use_cpu_feature(_FEATURE_AVX2);
+    return !!(regs1.ebx & AVX2);
   case avx512_common:
-    return _may_i_use_cpu_feature(_FEATURE_AVX512F);
+    return !!(regs1.ebx & AVX512F);
   case avx512_core:
-    return _may_i_use_cpu_feature(_FEATURE_AVX512F | _FEATURE_AVX512BW);
+    return !!(regs1.ebx & AVX512F) && !!(regs1.ebx & AVX512BW);
 #if defined(WITH_VNNI)
   case avx512_core_vnni:
-    return _may_i_use_cpu_feature(
-        _FEATURE_AVX512F | _FEATURE_AVX512BW | _FEATURE_AVX512_VNNI);
+    return !!(regs1.ebx & AVX512F) && !!(regs1.ebx & AVX512BW)
+        && !!(regs1.ecx & AVX512VNNI);
 #endif
   default:
     return false;
   }
   return false;
-#else
-  // TODO
-  return true;
-#endif
 }
 
 // CPU V length by byte
