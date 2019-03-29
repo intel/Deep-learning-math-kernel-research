63f4d49bf53773e006bc2b3e1457000376a991a3
fengyuan <feng1.yuan@intel.com>
2019-03-29 21:11:14 +0800

    conv: scripts: supply INT8 wino Or cases
    Signed-off-by: fengyuan <feng1.yuan@intel.com>

--- scripts/best_configs/resnet-n1-8180-1s-wino-int8.sh
+++ scripts/best_configs/resnet-n1-8180-1s-wino-int8.sh
@@ -19,5 +19,11 @@ NSOCKETS=1 ./scripts/run.sh -c -i512 -h7 -o512 -H7 -n1 --tile-size=4 --execution
 # a161 Ir case modified from resnet50_res2a_branch2b
 NSOCKETS=1 ./scripts/run.sh -c -i66 -h56 -o64 -H56 -n1 --tile-size=6 --execution-mode=0xa161 --blk-i=5 --blk-o=1 --flt-o=2 --flt-t=7 --pat-o=1 --output-as-blocked=true $COMMON --input-format=nchw --weights-format=oihw --data-type-cfg=U8F32S8F32 --sampling-kind=2
 
+# a161 Or case modified from resnet50_res2a_branch2b
+NSOCKETS=1 ./scripts/run.sh -c -i64 -h56 -o66 -H56 -n1 --tile-size=6 --execution-mode=0xa161 --blk-i=4 --blk-o=1 --flt-o=1 --flt-t=7 --pat-o=1 --output-as-blocked=true $COMMON --input-format=nchw --weights-format=oihw --output-format=nchw --data-type-cfg=U8F32S8F32 --sampling-kind=2
+
 # a133 Ir case modified from resnet50_res5a_branch2b
 NSOCKETS=1 ./scripts/run.sh -c -i516 -h7 -o512 -H7 -n1 --tile-size=4 --execution-mode=0xa133 --blk-i=33 --blk-o=1 --flt-o=2 --flt-t=8 $COMMON --input-format=nchw --weights-format=oihw --data-type-cfg=U8F32S8F32 --sampling-kind=2
+
+# a133 Or case modified from resnet50_res5a_branch2b
+NSOCKETS=1 ./scripts/run.sh -c -i512 -h7 -o516 -H7 -n1 --tile-size=4 --execution-mode=0xa133 --blk-i=32 --blk-o=1 --flt-o=1 --flt-t=8 $COMMON --input-format=nchw --weights-format=oihw --output-format=nchw --data-type-cfg=U8F32S8F32 --sampling-kind=2
