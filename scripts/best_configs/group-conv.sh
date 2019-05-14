e0bd78a17639afad1848ab2a6d9b38265af0d628
Yong Wu <yong.wu@intel.com>
2019-05-14 11:39:00 +0800

    test: add test case for direct-vmg
    Signed-off-by: Yong Wu <yong.wu@intel.com>

--- scripts/best_configs/group-conv.sh
+++ scripts/best_configs/group-conv.sh
@@ -3,7 +3,7 @@
 source ./scripts/best_configs/common.sh $@
 COMMON="$COMMON --f16c-opt=1"
 
-# d060
+# direct: d060
 NSOCKETS=1 ./scripts/run.sh -c -g32 -i128 -h56 -o128 -H56 -n1 --blk-i=1 --blk-o=1 --flt-o=1 --flt-t=28 -p1 -P1 -adirect --execution-mode=0xd060 --pat-o=1 $COMMON --weights-format=ghwio --input-format=nhwc --output-format=nhwc -b1
 
 NSOCKETS=1 ./scripts/run.sh -c -g32 -i256 -h28 -o256 -H28 -n1 --blk-i=1 --blk-o=1 --flt-o=1 --flt-t=28 -p1 -P1 -adirect --execution-mode=0xd060 --pat-o=1 $COMMON --weights-format=ghwio --input-format=nhwc --output-format=nhwc -b1
@@ -13,7 +13,7 @@ NSOCKETS=1 ./scripts/run.sh -c -g32 -i512 -h14 -o512 -H14 -n1 --blk-i=1 --blk-o=
 NSOCKETS=1 ./scripts/run.sh -c -g32 -i1024 -h7 -o1024 -H7 -n1 --blk-i=2 --blk-o=1 --flt-o=2 --flt-t=7 -p1 -P1 -adirect --execution-mode=0xd060 --pat-o=1 $COMMON --weights-format=ghwio --input-format=nChw16c --output-format=nChw16c -b1
 
 
-# a060
+# direct: a060
 NSOCKETS=1 ./scripts/run.sh -c -g32 -i128 -h56 -o128 -H56 -n1 --blk-i=1 --blk-o=1 --flt-o=1 --flt-t=14 -p1 -P1 -adirect --execution-mode=0xa060 --pat-o=1 $COMMON --weights-format=ghwio --input-format=nhwc --output-format=nhwc -b1
 
 NSOCKETS=1 ./scripts/run.sh -c -g32 -i256 -h28 -o256 -H28 -n1 --blk-i=1 --blk-o=1 --flt-o=1 --flt-t=14 -p1 -P1 -adirect --execution-mode=0xa060 --pat-o=1 $COMMON --weights-format=ghwio --input-format=nhwc --output-format=nhwc -b1
@@ -21,3 +21,10 @@ NSOCKETS=1 ./scripts/run.sh -c -g32 -i256 -h28 -o256 -H28 -n1 --blk-i=1 --blk-o=
 NSOCKETS=1 ./scripts/run.sh -c -g32 -i512 -h14 -o512 -H14 -n1 --blk-i=1 --blk-o=1 --flt-o=1 --flt-t=14 -p1 -P1 -adirect --execution-mode=0xa060 --pat-o=1 $COMMON --weights-format=ghwio --input-format=nChw16c --output-format=nChw16c -b1
 
 NSOCKETS=1 ./scripts/run.sh -c -g32 -i1024 -h7 -o1024 -H7 -n1 --blk-i=2 --blk-o=1 --flt-o=2 --flt-t=7 -p1 -P1 -adirect --execution-mode=0xa060 --pat-o=1 $COMMON --weights-format=ghwio --input-format=nChw16c --output-format=nChw16c -b1
+
+# direct_vmg
+NSOCKETS=1 ./scripts/run.sh -c -g32 -i128 -h56 -o128 -H56 -n1 --blk-i=1 --blk-o=1 --flt-o=1 --flt-t=14 -p1 -P1 -adirect_vmg --execution-mode=0xa060 --pat-o=1 $COMMON --weights-format=ghwio --input-format=nChw16c --output-format=nChw16c -b1
+
+NSOCKETS=1 ./scripts/run.sh -c -g32 -i256 -h28 -o256 -H28 -n1 --blk-i=1 --blk-o=1 --flt-o=1 --flt-t=14 -p1 -P1 -adirect_vmg --execution-mode=0xa060 --pat-o=1 $COMMON --weights-format=ghwio --input-format=nChw16c --output-format=nChw16c -b1
+
+
