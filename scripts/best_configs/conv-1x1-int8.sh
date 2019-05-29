faf5c4663b5addb056cc8624d16eb3a2822b9dc5
Yong Wu <yong.wu@intel.com>
2019-05-30 01:04:56 +0800

    tests: add conv1x1-int8 test cases, fine tune options
    Signed-off-by: Yong Wu <yong.wu@intel.com>

---
+++ scripts/best_configs/conv-1x1-int8.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+source ./scripts/best_configs/common.sh $@
+COMMON="$COMMON --data-type-cfg=U8F32S8F32 --sampling-kind=2 "
+
+# clx low bin, 28c
+
+# direct 1x1, s=1
+NSOCKETS=1 ./scripts/run.sh -c -i1024 -h28 -w28 -o2048 -H28 -W28 -k1 -K1 -s1 -S1 -p0 -P0 -n1 -adirect_1x1 --execution-mode=0xc160 --blk-i=16 --blk-o=2 --pat-o=1 --flt-o=2 --flt-t=14 --pat-i=4   $COMMON
+# 10.8 tflops, ref (7.6)
+NSOCKETS=1 ./scripts/run.sh -c -i1024 -h28 -w28 -o2048 -H28 -W28 -k1 -K1 -s1 -S1 -p0 -P0 -n64 -adirect_1x1 --execution-mode=0xc160 --blk-i=64 --blk-o=16 --pat-o=1 --flt-o=2 --flt-t=12 --pat-i=1   $COMMON
+# 10.9 tflops(12)
+
+# direct, s=1
+NSOCKETS=1 ./scripts/run.sh -c -i1024 -h28 -w28 -o2048 -H28 -W28 -k1 -K1 -s1 -S1 -p0 -P0 -n1 -adirect --execution-mode=0xd160 --blk-i=16 --blk-o=1 --pat-o=2 --flt-o=2 --flt-t=14 --pat-i=4   $COMMON
+# 10.0, ref (7.6tflops)
+NSOCKETS=1 ./scripts/run.sh -c -i1024 -h28 -w28 -o2048 -H28 -W28 -k1 -K1 -s1 -S1 -p0 -P0 -n64 -adirect --execution-mode=0xd160 --blk-i=64 --blk-o=4 --pat-o=2 --flt-o=2 --flt-t=14 --pat-i=1   $COMMON
+# 9.22 tflops, ref(12 tflops)
+
+# direct 1x1, s=2
+NSOCKETS=1 ./scripts/run.sh -c -i1024 -h28 -w28 -o2048 -H14 -W14 -k1 -K1 -s2 -S2 -p0 -P0 -n1 -adirect_1x1 --execution-mode=0xb161 --blk-i=32 --blk-o=2 --pat-o=1 --flt-o=2 --flt-t=14 --pat-i=2   $COMMON
+# 7.0 tflops, ref (7.7 tflops)
+NSOCKETS=1 ./scripts/run.sh -c -i1024 -h28 -w28 -o2048 -H14 -W14 -k1 -K1 -s2 -S2 -p0 -P0 -n64 -adirect_1x1 --execution-mode=0xb161 --blk-i=64 --blk-o=2 --pat-o=1 --flt-o=2 --flt-t=14 --pat-i=1   $COMMON
+# 10.03 tflops. ref (9.7 tflops)
+
+# direct s=2
+NSOCKETS=1 ./scripts/run.sh -c -i1024 -h28 -w28 -o2048 -H14 -W14 -k1 -K1 -s2 -S2 -p0 -P0 -n1 -adirect --execution-mode=0xd160 --blk-i=16 --blk-o=2 --pat-o=1 --flt-o=2 --flt-t=14 --pat-i=4   $COMMON
+# 6.64 (7.7)
+NSOCKETS=1 ./scripts/run.sh -c -i1024 -h28 -w28 -o2048 -H14 -W14 -k1 -K1 -s2 -S2 -p0 -P0 -n64 -adirect --execution-mode=0xd160 --blk-i=64 --blk-o=2 --pat-o=1 --flt-o=2 --flt-t=14 --pat-i=1   $COMMON
+# 8.9 (9.7)
+
+
--- scripts/best_configs/resnet-n64-8180-1s-wino-int8.sh
+++ scripts/best_configs/resnet-n64-8180-1s-wino-int8.sh
@@ -14,4 +14,4 @@ NSOCKETS=1 ./scripts/run.sh -c -i128 -h28 -o128 -H28 -n64 --tile-size=6 --execut
 # resnet50_res4a_branch2b
 NSOCKETS=1 ./scripts/run.sh -c -i256 -h14 -o256 -H14 -n64 --tile-size=6 --execution-mode=0xa161 --blk-i=16 --blk-o=2 --flt-o=2 --flt-t=14 --pat-o=4 $COMMON --data-type-cfg=U8F32S8F32 --sampling-kind=2
 # resnet50_res5a_branch2b
-NSOCKETS=1 ./scripts/run.sh -c -i512 -h7 -o512 -H7 -n64 --tile-size=6 --execution-mode=0xa133 --blk-i=32 --blk-o=2 --flt-o=2 --flt-t=14 --pat-i=1 --pat-o=8 $COMMON --data-type-cfg=U8F32S8F32 --sampling-kind=2
+NSOCKETS=1 ./scripts/run.sh -c -i512 -h7 -o512 -H7 -n64 --tile-size=6 --execution-mode=0xa133 --blk-i=32 --blk-o=2 --flt-o=2 --flt-t=14 --pat-i=1 --pat-o=1 $COMMON --data-type-cfg=U8F32S8F32 --sampling-kind=2
