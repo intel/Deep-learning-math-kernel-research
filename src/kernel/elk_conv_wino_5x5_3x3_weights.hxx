3e19253cf9082a423a4c4190075226ed43c439a1
Yong Wu <yong.wu@intel.com>
2019-10-02 04:07:20 +0800

    wino: weights-transform: remove error macro definition
    Signed-off-by: Yong Wu <yong.wu@intel.com>

--- src/kernel/elk_conv_wino_5x5_3x3_weights.hxx
+++ src/kernel/elk_conv_wino_5x5_3x3_weights.hxx
@@ -23,19 +23,19 @@ namespace euler {
 
 #define AVX512_CALCULATE_W_5(n)                                                \
   t0##n = (c0 + c1 + c2);                                                      \
-  _mm<V>::store_ps(T(0, n), t0##n);                                            \
+  _mm<V>::store_ps(atweights[0][n][_V], t0##n);                                \
   t1##n = (c0 - c1 + c2);                                                      \
-  _mm<V>::store_ps(T(1, n), t1##n);                                            \
+  _mm<V>::store_ps(atweights[1][n][_V], t1##n);                                \
   t2##n = (r1_10 * c0 + (r1_5 * c1 + (r2_5 * c2)));                            \
-  _mm<V>::store_ps(T(2, n), t2##n);                                            \
+  _mm<V>::store_ps(atweights[2][n][_V], t2##n);                                \
   t3##n = (r1_5 * c1 - (r1_10 * c0 + (r2_5 * c2)));                            \
-  _mm<V>::store_ps(T(3, n), t3##n);                                            \
+  _mm<V>::store_ps(atweights[3][n][_V], t3##n);                                \
   t4##n = (r8_5 * c0 + (r4_5 * c1 + (r2_5 * c2)));                             \
-  _mm<V>::store_ps(T(4, n), t4##n);                                            \
+  _mm<V>::store_ps(atweights[4][n][_V], t4##n);                                \
   t5##n = (r4_5 * c1 - (r8_5 * c0 + (r2_5 * c2)));                             \
-  _mm<V>::store_ps(T(5, n), t5##n);                                            \
+  _mm<V>::store_ps(atweights[5][n][_V], t5##n);                                \
   t6##n = (r9_2 * c2);                                                         \
-  _mm<V>::store_ps(T(6, n), t6##n);
+  _mm<V>::store_ps(atweights[6][n][_V], t6##n);
 
 template <typename WeightsType, int V>
 struct elk_conv_wino_trans_weights<float, WeightsType, ISA_SKX_AVX512,
@@ -66,19 +66,15 @@ struct elk_conv_wino_trans_weights<float, WeightsType, ISA_SKX_AVX512,
         t40, t41, t42, t43, t44, t45, t46, t50, t51, t52, t53, t54, t55, t56,
         t60, t61, t62, t63, t64, t65, t66;
 
-#undef F
-#undef T
-#define F(h, w) aweights[h][w][_V]
-#define T(h, w) atweights[h][w][_V]
 
 #undef f
 #undef LOAD 
 #define f(m, n) f##m##n
 #define LOAD(m, n)                                                             \
   if (std::is_same<WeightsType, float>::value)                                 \
-    f(m, n) = _mm<V>::load_ps(F(m, n));                                        \
+    f(m, n) = _mm<V>::load_ps(aweights[m][n][_V]);                             \
   else {                                                                       \
-    auto f16 = _mm<V / 2>::load_si256((__m256i *)F(m, n));                     \
+    auto f16 = _mm<V / 2>::load_si256((__m256i *)aweights[m][n][_V]);          \
     f(m, n) = _mm<V>::cvtph_ps(f16);                                           \
   }
 
@@ -146,19 +142,19 @@ struct elk_conv_wino_trans_weights<float, WeightsType, ISA_SKX_AVX512,
       c1 = (r1_45 * f02 + r4_45 * f22);
       c2 = (r16_45 * f02 + r4_45 * f22);
       t06 = (r2_9 * f12 + c0);
-      _mm<V>::store_ps(T(0, 6), t06);
+      _mm<V>::store_ps(atweights[0][6][_V], t06);
       t16 = -(r2_9 * f12 - c0);
-      _mm<V>::store_ps(T(1, 6), t16);
+      _mm<V>::store_ps(atweights[1][6][_V], t16);
       t26 = (r2_45 * f12 + c1);
-      _mm<V>::store_ps(T(2, 6), t26);
+      _mm<V>::store_ps(atweights[2][6][_V], t26);
       t36 = (r2_45 * f12 - c1);
-      _mm<V>::store_ps(T(3, 6), t36);
+      _mm<V>::store_ps(atweights[3][6][_V], t36);
       t46 = (r8_45 * f12 + c2);
-      _mm<V>::store_ps(T(4, 6), t46);
+      _mm<V>::store_ps(atweights[4][6][_V], t46);
       t56 = (r8_45 * f12 - c2);
-      _mm<V>::store_ps(T(5, 6), t56);
+      _mm<V>::store_ps(atweights[5][6][_V], t56);
       t66 = f22;
-      _mm<V>::store_ps(T(6, 6), t66);
+      _mm<V>::store_ps(atweights[6][6][_V], t66);
     }
   }
 }; // elk_conv_wino_trans_weights
