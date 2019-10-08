eb5b3be203a3ff378e9ee4e8d7eeddab209b4558
Yong Wu <yong.wu@intel.com>
2019-10-08 09:54:58 +0800

    mdarray: support AMD (anonymous multi-dimension array)
    Signed-off-by: Yong Wu <yong.wu@intel.com>

--- src/common/el_mdarray.hpp
+++ src/common/el_mdarray.hpp
@@ -99,6 +99,10 @@ template <typename F, const int N> class mdarray {
   assert((ptr) != nullptr);                                                    \
   auto &array = *reinterpret_cast<type(*) __SQUARE(n, __VA_ARGS__)>(ptr)
 
+// anonymous md
+#define __AMD(type, n, ptr, ...)                                               \
+  (*reinterpret_cast<type(*) __SQUARE(n, __VA_ARGS__)>(ptr))
+
 #define md1(array, ...)  ((array)__SQUARE(1, __VA_ARGS__))
 #define md2(array, ...)  ((array)__SQUARE(2, __VA_ARGS__))
 #define md3(array, ...)  ((array)__SQUARE(3, __VA_ARGS__))
@@ -125,10 +129,40 @@ template <typename F, const int N> class mdarray {
 #define md24(array, ...) ((array)__SQUARE(24, __VA_ARGS__))
 #define md25(array, ...) ((array)__SQUARE(25, __VA_ARGS__))
 
-#else
+#define amd1(args, ...)  (AMD1 args __SQUARE(1, __VA_ARGS__))
+#define amd2(args, ...)  (AMD2 args __SQUARE(2, __VA_ARGS__))
+#define amd3(args, ...)  (AMD3 args __SQUARE(3, __VA_ARGS__))
+#define amd4(args, ...)  (AMD4 args __SQUARE(4, __VA_ARGS__))
+#define amd5(args, ...)  (AMD5 args __SQUARE(5, __VA_ARGS__))
+#define amd6(args, ...)  (AMD6 args __SQUARE(6, __VA_ARGS__))
+#define amd7(args, ...)  (AMD7 args __SQUARE(7, __VA_ARGS__))
+#define amd8(args, ...)  (AMD8 args __SQUARE(8, __VA_ARGS__))
+#define amd9(args, ...)  (AMD9 args __SQUARE(9, __VA_ARGS__))
+#define amd10(args, ...) (AMD10 args __SQUARE(10, __VA_ARGS__))
+#define amd11(args, ...) (AMD11 args __SQUARE(11, __VA_ARGS__))
+#define amd12(args, ...) (AMD12 args __SQUARE(12, __VA_ARGS__))
+#define amd13(args, ...) (AMD13 args __SQUARE(13, __VA_ARGS__))
+#define amd14(args, ...) (AMD14 args __SQUARE(14, __VA_ARGS__))
+#define amd15(args, ...) (AMD15 args __SQUARE(15, __VA_ARGS__))
+#define amd16(args, ...) (AMD16 args __SQUARE(16, __VA_ARGS__))
+#define amd17(args, ...) (AMD17 args __SQUARE(17, __VA_ARGS__))
+#define amd18(args, ...) (AMD18 args __SQUARE(18, __VA_ARGS__))
+#define amd19(args, ...) (AMD19 args __SQUARE(19, __VA_ARGS__))
+#define amd20(args, ...) (AMD20 args __SQUARE(20, __VA_ARGS__))
+#define amd21(args, ...) (AMD21 args __SQUARE(21, __VA_ARGS__))
+#define amd22(args, ...) (AMD22 args __SQUARE(22, __VA_ARGS__))
+#define amd23(args, ...) (AMD23 args __SQUARE(23, __VA_ARGS__))
+#define amd24(args, ...) (AMD24 args __SQUARE(24, __VA_ARGS__))
+#define amd25(args, ...) (AMD25 args __SQUARE(25, __VA_ARGS__))
+
+#else // (__INTEL_COMPILER || __GCC_COMPILER)
 
 #define __MD(type, n, array, ptr, ...)                                         \
   mdarray<type, n> array(ptr, __COMMA(n, __VA_ARGS__))
+
+// anonymous md
+#define __AMD(type, n, ptr, ...)                                               \
+  mdarray<type, n>(ptr, __COMMA(n, __VA_ARGS__))
 
 #define md1(array, ...)  (array(__COMMA(1, __VA_ARGS__)))
 #define md2(array, ...)  (array(__COMMA(2, __VA_ARGS__)))
@@ -155,6 +189,32 @@ template <typename F, const int N> class mdarray {
 #define md23(array, ...) (array(__COMMA(23, __VA_ARGS__)))
 #define md24(array, ...) (array(__COMMA(24, __VA_ARGS__)))
 #define md25(array, ...) (array(__COMMA(25, __VA_ARGS__)))
+
+#define amd1(args, ...)  (AMD1 args (__COMMA(1, __VA_ARGS__)))
+#define amd2(args, ...)  (AMD2 args (__COMMA(2, __VA_ARGS__)))
+#define amd3(args, ...)  (AMD3 args (__COMMA(3, __VA_ARGS__)))
+#define amd4(args, ...)  (AMD4 args (__COMMA(4, __VA_ARGS__)))
+#define amd5(args, ...)  (AMD5 args (__COMMA(5, __VA_ARGS__)))
+#define amd6(args, ...)  (AMD6 args (__COMMA(6, __VA_ARGS__)))
+#define amd7(args, ...)  (AMD7 args (__COMMA(7, __VA_ARGS__)))
+#define amd8(args, ...)  (AMD8 args (__COMMA(8, __VA_ARGS__)))
+#define amd9(args, ...)  (AMD9 args (__COMMA(9, __VA_ARGS__)))
+#define amd10(args, ...) (AMD10 args (__COMMA(10, __VA_ARGS__)))
+#define amd11(args, ...) (AMD11 args (__COMMA(11, __VA_ARGS__)))
+#define amd12(args, ...) (AMD12 args (__COMMA(12, __VA_ARGS__)))
+#define amd13(args, ...) (AMD13 args (__COMMA(13, __VA_ARGS__)))
+#define amd14(args, ...) (AMD14 args (__COMMA(14, __VA_ARGS__)))
+#define amd15(args, ...) (AMD15 args (__COMMA(15, __VA_ARGS__)))
+#define amd16(args, ...) (AMD16 args (__COMMA(16, __VA_ARGS__)))
+#define amd17(args, ...) (AMD17 args (__COMMA(17, __VA_ARGS__)))
+#define amd18(args, ...) (AMD18 args (__COMMA(18, __VA_ARGS__)))
+#define amd19(args, ...) (AMD19 args (__COMMA(19, __VA_ARGS__)))
+#define amd20(args, ...) (AMD20 args (__COMMA(20, __VA_ARGS__)))
+#define amd21(args, ...) (AMD21 args (__COMMA(21, __VA_ARGS__)))
+#define amd22(args, ...) (AMD22 args (__COMMA(22, __VA_ARGS__)))
+#define amd23(args, ...) (AMD23 args (__COMMA(23, __VA_ARGS__)))
+#define amd24(args, ...) (AMD24 args (__COMMA(24, __VA_ARGS__)))
+#define amd25(args, ...) (AMD25 args (__COMMA(25, __VA_ARGS__)))
 
 #endif
 
@@ -184,5 +244,30 @@ template <typename F, const int N> class mdarray {
 #define MD24(type, array, ptr, ...) __MD(type, 24, array, ptr, __VA_ARGS__)
 #define MD25(type, array, ptr, ...) __MD(type, 25, array, ptr, __VA_ARGS__)
 
+#define AMD1(type,  ptr, ...) __AMD(type, 1,  ptr, __VA_ARGS__)
+#define AMD2(type,  ptr, ...) __AMD(type, 2,  ptr, __VA_ARGS__)
+#define AMD3(type,  ptr, ...) __AMD(type, 3,  ptr, __VA_ARGS__)
+#define AMD4(type,  ptr, ...) __AMD(type, 4,  ptr, __VA_ARGS__)
+#define AMD5(type,  ptr, ...) __AMD(type, 5,  ptr, __VA_ARGS__)
+#define AMD6(type,  ptr, ...) __AMD(type, 6,  ptr, __VA_ARGS__)
+#define AMD7(type,  ptr, ...) __AMD(type, 7,  ptr, __VA_ARGS__)
+#define AMD8(type,  ptr, ...) __AMD(type, 8,  ptr, __VA_ARGS__)
+#define AMD9(type,  ptr, ...) __AMD(type, 9,  ptr, __VA_ARGS__)
+#define AMD10(type, ptr, ...) __AMD(type, 10, ptr, __VA_ARGS__)
+#define AMD11(type, ptr, ...) __AMD(type, 11, ptr, __VA_ARGS__)
+#define AMD12(type, ptr, ...) __AMD(type, 12, ptr, __VA_ARGS__)
+#define AMD13(type, ptr, ...) __AMD(type, 13, ptr, __VA_ARGS__)
+#define AMD14(type, ptr, ...) __AMD(type, 14, ptr, __VA_ARGS__)
+#define AMD15(type, ptr, ...) __AMD(type, 15, ptr, __VA_ARGS__)
+#define AMD16(type, ptr, ...) __AMD(type, 16, ptr, __VA_ARGS__)
+#define AMD17(type, ptr, ...) __AMD(type, 17, ptr, __VA_ARGS__)
+#define AMD18(type, ptr, ...) __AMD(type, 18, ptr, __VA_ARGS__)
+#define AMD19(type, ptr, ...) __AMD(type, 19, ptr, __VA_ARGS__)
+#define AMD20(type, ptr, ...) __AMD(type, 20, ptr, __VA_ARGS__)
+#define AMD21(type, ptr, ...) __AMD(type, 21, ptr, __VA_ARGS__)
+#define AMD22(type, ptr, ...) __AMD(type, 22, ptr, __VA_ARGS__)
+#define AMD23(type, ptr, ...) __AMD(type, 23, ptr, __VA_ARGS__)
+#define AMD24(type, ptr, ...) __AMD(type, 24, ptr, __VA_ARGS__)
+#define AMD25(type, ptr, ...) __AMD(type, 25, ptr, __VA_ARGS__)
 
 } // namespace euler
